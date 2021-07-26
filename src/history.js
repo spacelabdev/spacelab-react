8201ea18705d4e7901e89e513cb756fd3ba8bd4d
Brandon Cantello <bcantello@gmail.com>
2021-07-26 15:38:20 -0700

    Boilerplate cleanup. Adds some additional boilerplate.

---
+++ src/history.js
@@ -0,0 +1,7 @@
+/**
+ * Creates a history object that can be used to navigate the React App from outside of a component.
+ */
+
+import {createBrowserHistory} from 'history';
+
+export default createBrowserHistory();
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router} from 'react-router';
 import './index.css';
 import App from './App';
+import history from "./history";
 import reportWebVitals from './reportWebVitals';
 
+require('dotenv').config();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Router history={history}>
+        <App />
+    </Router>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
