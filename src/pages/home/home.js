544674b08de68c1260ca7c09b5d4b8e681cdfea0
Anna <anna.chowattanakul@gmail.com>
2021-07-26 22:13:36 -0400

    Add scss to the repo

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,12 +1,14 @@
 import React from "react";
-import './home.css';
+import "./home.scss";
 
 /**
  * @returns {JSX.Element}
  * @constructor
  */
 export default function Home() {
-	return (
-		<>Home Coming Soon!</>
-	);
-};
+  return (
+    <>
+      <h1>Home Coming Soon!</h1>
+    </>
+  );
+}
