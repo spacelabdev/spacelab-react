43961e598d0ce7adfaac7ed532c0013d885e75cb
Brandon Cantello <bcantello@gmail.com>
2021-08-03 12:59:09 -0700

    tracks package lock

--- src/pages/glossary/glossaryhelper.js
+++ src/pages/glossary/glossaryhelper.js
@@ -1,4 +1,59 @@
-export const glossaryTerms = {
+/**
+ * Returns glossary terms with alphabetic index less than or equal to 9, which is A-I.
+ * @returns {*[]}
+ */
+export const findFirstThird = () => {
+	const glossaryArray = [];
+	for (let i = 0; i < glossaryTermsArray.length; i++) {
+		if (glossaryTermsArray[i][2] <= 9) {
+			glossaryArray.push(glossaryTermsArray[i]);
+		}
+	}
+	return glossaryArray;
+};
+
+/**
+ * Returns glossary terms with alphabetic index between 10 and 19, which is L-S.
+ * @returns {*[]}
+ */
+export const findMiddleThird = () => {
+	const glossaryArray = [];
+	for (let i = 0; i < glossaryTermsArray.length; i++) {
+		if (glossaryTermsArray[i][2] > 9 && glossaryTermsArray[i][2] <= 19) {
+			glossaryArray.push(glossaryTermsArray[i]);
+		}
+	}
+	return glossaryArray;
+};
+
+/**
+ * Returns glossary terms with alphabetic index greater than 19, which is T-Z.
+ * @returns {*[]}
+ */
+export const findLastThird = () => {
+	const glossaryArray = [];
+	for (let i = 0; i < glossaryTermsArray.length; i++) {
+		if (glossaryTermsArray[i][2] > 19) {
+			glossaryArray.push(glossaryTermsArray[i]);
+		}
+	}
+	return glossaryArray;
+};
+
+/**
+ * Dictionary of Glossary terms organized by key value pairs where the key is the term and the value is the definition.
+ * @type {{"Black Body": string, "Stellar Limb": string, "Stellar Variability": string,
+ * "Hydrogen-Helium Envelope": string, Eccentricity: string, "Limb Darkening": string, "Secondary Eclipse": string,
+ * "Log g [cgs]": string, "Relativistic Beaming": string, "Galactic Plane": string, "Eclipsing Binaries": string,
+ * Transits: string, "Disintegrating Planets": string, "Tidal circularization": string, "Warm Jupiter": string,
+ * "Transit timing variation": string, "Astrometric solution": string, "Effective Temperature": string,
+ * "Scale Heights": string, "Mutual Inclinations": string, "Insolation Flux": string,
+ * "Threshold Crossing Events (TCE)": string, "Hot Jupiter": string, "Main sequence star": string,
+ * "Stellar Disk": string, "Gravitational Lensing": string, "Super-Earths": string, "Transit Depth": string,
+ * "Orbital Inclination": string, "Transit Midpoint": string, Photoevaporation: string, "Three body resonance": string,
+ * "G-dwarf Star": string}}
+ */
+export const glossaryTermsDictionary = {
 	"Astrometric solution": "An astrometric solution is an optical technique used in astronomical imaging, "
 		+ "where \"solving\" an image means finding a match between the imaged stars and a star catalogue. "
 		+ "Therefore an astrometric solution is a model describing the astronomical position of each image pixel.",
@@ -86,3 +141,227 @@ export const glossaryTerms = {
 		+ "Jupiters, in which their orbital periods are between that of Jupiters (over 100 days) and "
 		+ "Hot Jupiters (1 to 10 days).",
 }
+
+/**
+ * Array of glossary term arrays. Each glossary term array consists of [term, definition, alphabetic index].
+ * Ex: [Astronomy, study of the stars, 1] Alphabetic index is 1 since the term starts with 'A'. Index of 'B' is 2, ...,
+ * and the index of 'Z' is 26.
+ * @type {((string|number)[]|string)[]}
+ */
+export const glossaryTermsArray = [
+	[
+		"Astrometric solution",
+		"An astrometric solution is an optical technique used in astronomical imaging, "
+		+ "where \"solving\" an image means finding a match between the imaged stars and a star catalogue. "
+		+ "Therefore an astrometric solution is a model describing the astronomical position of each image pixel.",
+		1
+	],
+	[
+		"Black Body",
+		"A black body is an idealized body or object that absorbs all incident electromagnetic radiation "
+		+ "(light) at any frequency. It can also emit radiation in any frequency.",
+		2
+	],
+	[
+		"Disintegrating Planets",
+		"A disintegrating planet is when a planet is losing dust and gas as it appears to "
+		+ "(or actually does) disintegrate, which can result in the light from a star behind an orbiting planet "
+		+ "becoming dimmed or blocked.",
+		4
+	],
+	[
+		"Eccentricity",
+		"The eccentricity of an exoplanet or orbiting object is a measure of how much the orbit of an "
+		+ "orbiting body deviates from a circle. An eccentricity of 0 would be a perfectly circular orbit, an "
+		+ "eccentricity of 1 is a parabolic orbit, anything in between is an elliptical orbit.",
+		5
+	],
+	[
+		"Eclipsing Binaries",
+		"An eclipsing binary star is one whose brightness varies because the two components pass in "
+		+ "front of each other. One start is \"eclipsed\" by the other.",
+		5
+	],
+	[
+		"Effective Temperature",
+		"The effective temperature of a star is the temperature of a black body with the same "
+		+ "luminosity per surface area as the star (defined by Stefan-Boltzmann Law). In essence, it's the surface "
+		+ "temperature of a star if we assume the star to be a perfect black body.",
+		5
+	],
+	[
+		"G-dwarf Star",
+		"A G-dwarf star, otherwise known as a yellow dwarf, is a type of main sequence star.",
+		7
+	],
+	[
+		"Galactic Plane",
+		"The galactic plane is the plane through which the majority of a disk-shaped galaxy's mass lies, "
+		+ "typically referring specifically to the Milky Way Galaxy. Directions perpendicular to galactic plane "
+		+ "define the galactic poles.",
+		7
+	],
+	[
+		"Gravitational Lensing",
+		"Gravitational lensing is when light from a star located behind a black hole or other "
+		+ "massive object is bent around the massive object, distorting the image of the star and allowing you to "
+		+ "see it even though it exists behind another object in the sky. In some cases it can result in multiple "
+		+ "images of the same object appearing in the region around a black hole.",
+		7
+	],
+	[
+		"Hot Jupiter",
+		"A hot jupiter is a class of giant exoplanets that resemble Jupiter and have very short orbital "
+		+ "periods, meaning that they are orbiting much closer to their star. According to CalTech, orbital "
+		+ "periods of Hot Jupiters are usually less than or equal to 10 days. They are easy to identify using "
+		+ "transit methods because of their proximity to stars.",
+		8
+	],
+	[
+		"Hydrogen-Helium Envelope",
+		"A hydrogen/helium envelope is like the \"envelope\" of gases surrounding gas giants, "
+		+ "and describes the pockets of gas surrounding certain types of exoplanets.",
+		8
+	],
+	[
+		"Insolation Flux",
+		"Insolation flux is the measure of the amount of solar radiation an exoplanet receives.",
+		9
+	],
+	[
+		"Limb Darkening",
+		"Limb darkening is the optical effect seen in stars where the outer edge of the star appears "
+		+ "dimmer or darker, while the center of the star appears brighter. The darker outer edge is referred to "
+		+ "as the stellar limb.",
+		12
+	],
+	[
+		"Log g [cgs]",
+		"Log g in units of [cgs] describes the surface gravity on an exoplanet, otherwise known as the "
+		+ "acceleration due to gravity.",
+		12
+	],
+	[
+		"Main sequence star",
+		"A main sequence star is a star in the middle of its life cycle that is still fusing "
+		+ "hydrogen to form helium, meaning it still has hydrogen reserves.",
+		13
+	],
+	[
+		"Mutual Inclinations",
+		"Orbital inclination is the tilt of an orbit around a celestial body with reference to a "
+		+ "particular and arbitrarily chosen plane. Mutual inclination, therefore, is a measure of the "
+		+ "inclinations of two orbiting bodies",
+		13
+	],
+	[
+		"Orbital Inclination",
+		"Orbital inclination is the tilt of an object's orbit around a celestial body, expressed as "
+		+ "the angle between a reference plane and the axis of direction of the orbiting object, where the "
+		+ "reference plane is an arbitrary choice.",
+		15
+	],
+	[
+		"Photoevaporation",
+		"Photoevaporation is the process in which radiation ionises gas and causes it to disperse away "
+		+ "from the ionising source of the radiation.",
+		16
+	],
+	[
+		"Relativistic Beaming",
+		"In relativistic frames, relativistic beaming is when a light source is emitted in one "
+		+ "frame of reference, but that frame of reference is moving relative to another, therefore shifting the "
+		+ "appearance of the light. Occurs with bodies emitting light that are moving near the speed of light. "
+		+ "Therefore, if a star were moving past you at near the speed of light, it's light would appear to become "
+		+ "focused in the direction of motion, whereas if you were at rest relative to that star it's light would "
+		+ "emit in all directions.",
+		18
+	],
+	[
+		"Scale Heights",
+		"Scale height refers to the distance over which a physical variable decreases by a factor of e. "
+		+ "In terms of a galaxy, the scale height describes the height above the galactic disk/ galactic plane "
+		+ "\"at which the density of a particular constituent of the disk has declined by e\".",
+		19
+	],
+	[
+		"Secondary Eclipse",
+		"A secondary eclipse is when a planet passes behind a star and the light from the planet is dimmed.",
+		19
+	],
+	[
+		"Stellar Disk",
+		"A stellar disk is the ring-shaped accumulation of matter that orbits around a star.",
+		19
+	],
+	[
+		"Stellar Limb",
+		"The stellar limb is the outer edge of a star, which appears darker due to the effect of limb darkening.",
+		19
+	],
+	[
+		"Stellar Variability",
+		"Stellar variability describes how stars change throughout their lifetimes, "
+		+ "such as variations in brightness and size depending on their position in the sequence of star-lives.",
+		19
+	],
+	[
+		"Super-Earths",
+		"Super-Earths are exoplanets whose mass is higher than Earth's, but still significantly smaller "
+		+ "than that of the ice giants.",
+		19
+	],
+	[
+		"Three body resonance",
+		"Orbital resonance is when orbiting bodies exert periodic gravitational influence on each "
+		+ "other during orbits. Three-body resonance describes when orbital resonance occurs between three orbiting "
+		+ "bodies (such as in the infamous three-body problem).",
+		20
+	],
+	[
+		"Threshold Crossing Events (TCE)",
+		"A TCE is a potentially periodic decrease in the brightness of a star that has "
+		+ "been detected in a search for exoplanets using a light curve.",
+		20
+	],
+	[
+		"Tidal circularization",
+		"Tidal circularization is the process in which the orbit of an orbiting body gradually "
+		+ "becomes more circular over time. It's an effect of the tidal forces between an orbiting body and the "
+		+ "object that it orbits, where the eccentricity (measure of how much an orbit deviates from a circle) "
+		+ "decreases over time.",
+		20
+	],
+	[
+		"Transit Depth",
+		"A transit is an object that passes in front of a star, such that the transit depth is the ratio "
+		+ "of the surface area of the star's disk blocked out by the planet's disk. In other words, the ratio of "
+		+ "the area of the planet to the area of the star, which can be used to calculate the size of the planet.",
+		20
+	],
+	[
+		"Transit Midpoint",
+		"The transit midpoint of a transiting object is the average of the time from when the "
+		+ "exoplanet begins to cross the stellar limb to the time the candidate finishes crossing the stellar limb.",
+		20
+	],
+	[
+		"Transit timing variation",
+		"Transit timing variation is a method for detecting exoplanets by observing variations "
+		+ "in the timing of a transit. It is based on the idea that when a planet crosses a star and gets tugged on by "
+		+ "a second planet, it will speed up or slow down, and therefore won't cross the star when predicted. The "
+		+ "difference in timing allows for the prediction of a second planet.",
+		20
+	],
+	[
+		"Transits",
+		"A transit is an object or exoplanet passing in front of a star and dimming its light.",
+		20
+	],
+	[
+		"Warm Jupiter",
+		"Warm Jupiters are a type of planet classified as being between Jupiter-like planets and Hot Jupiters, in "
+		+ "which their orbital periods are between that of Jupiters (over 100 days) and Hot Jupiters (1 to 10 days).",
+		23
+	],
+];
