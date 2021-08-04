a749e3d8d20c1345cc171da3ea960254b8735674
kath <khackethal.freelance@gmail.com>
2021-08-04 17:43:51 +0100

    fixing spacing

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -21,78 +21,67 @@ export default function About() {
         <h1 id="main-header">We're Space Lab</h1>
         <h4> Meet the team!</h4>
         <div className="team-picture-div">
-          {teamImages.map(image => 
-            <div><img className="team-picture" src={image} alt="placeholder" /></div>
-          )}
+            {teamImages.map(image => 
+              <div><img className="team-picture" src={image} alt="placeholder" /></div>
+            )}
         </div>
       </section>
 
       <section className="section-two">
         <div className="inner-section-two">
-        <div className="left-div-section-two">
-          <div>
-          <img className="section-two-image" src={PlaceHolder} alt="placeholder" />
+          <div className="left-div-section-two">
+            <div>
+            <img className="section-two-image" src={PlaceHolder} alt="placeholder" />
+            </div>
           </div>
-          </div>
-
-        <div className="right-div-section-two">
-          <div>
-          <h1 id="second-header">We're Space Lab</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Vestibulum ligula nisi, tristique ut massa.
-          </p>
-          </div>
-          <div className="nested-div-two">
-          <div className="nested-inner-div">
-            <h4>Space Lab</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </div>
-
-          <div className="nested-inner-div">
-          <h4>Space Lab</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </div>
-          </div>
-
+          <div className="right-div-section-two">
+            <div>
+              <h1 id="second-header">We're Space Lab</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum ligula nisi, tristique ut massa.
+              </p>
+            </div>
+            <div className="nested-div-two">
+              <div className="nested-inner-div">
+                <h4>Space Lab</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </div>
+              <div className="nested-inner-div">
+                <h4>Space Lab</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </div>
+            </div>
           </div>
         </div>
       </section>
 
-      <section className="section-three">
+    <section className="section-three">
         <div className="section-three-inner-div">
           <div className="left-side-section-three">
-          <div id="faq">FAQ</div>
-          <div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Vestibulum ligula nisi, tristique ut massa.
+            <div id="faq">FAQ</div>
+            <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Vestibulum ligula nisi, tristique ut massa.
+            </div>
           </div>
-          </div>
-
         <div className="right-side-section-three">
           <div className="right-side-nested-div">
-          <div className="question-div">
-            <p>Extroplanet Question</p>
-            <p>Habitable Zone</p>
-            <p>Solar System</p>
-            <p>Space Lab Question</p>
+            <div className="question-div">
+              <p>Extroplanet Question</p>
+              <p>Habitable Zone</p>
+              <p>Solar System</p>
+              <p>Space Lab Question</p>
+            </div>
+            <div className="answer-div">
+              <p>Answer</p>
+              <p>Answer</p>
+              <p>Answer</p>
+              <p>Answer</p>
+            </div>
           </div>
-          <div className="answer-div">
-            <p>Answer</p>
-            <p>Answer</p>
-            <p>Answer</p>
-            <p>Answer</p>
-          </div>
-          </div>
-
-          </div>
-
         </div>
-
-
-      </section>
-
-      
-
+      </div>
+    </section>
 		</>
 	);
 };
