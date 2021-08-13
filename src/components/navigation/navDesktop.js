11e842f645606e2308f76ba9ff097745778cb7cc
Anna <anna.chowattanakul@gmail.com>
2021-08-12 22:24:45 -0400

    fix small things

--- src/components/navigation/navDesktop.js
+++ src/components/navigation/navDesktop.js
@@ -12,18 +12,18 @@ export default function NavDesktop() {
 					</Link>
 				</div>
 				<div className="nav-links">
-						<Link className={'nav-link'} to={'/about'} style={{textDecoration: 'none'}}>
-							About
-						</Link>
-						<Link className={'nav-link'} to={'/discovery'} style={{textDecoration: 'none'}}>
-							Discovery
-						</Link>
-						<Link className={'nav-link'} to={'/glossary'} style={{textDecoration: 'none'}}>
-							Glossary
-						</Link>
-						<Link className={'nav-link'} to={'/blog'} style={{textDecoration: 'none'}}>
-							Blog
-						</Link>
+					<Link className={'nav-link'} to={'/about'} style={{textDecoration: 'none'}}>
+						About
+					</Link>
+					<Link className={'nav-link'} to={'/discovery'} style={{textDecoration: 'none'}}>
+						Discovery
+					</Link>
+					<Link className={'nav-link'} to={'/glossary'} style={{textDecoration: 'none'}}>
+						Glossary
+					</Link>
+					<Link className={'nav-link'} to={'/blog'} style={{textDecoration: 'none'}}>
+						Blog
+					</Link>
 				</div>
 			</div>
 		</nav>
