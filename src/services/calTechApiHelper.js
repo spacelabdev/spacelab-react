0a609ce9578a3c3e998c6feb2775e0e8a9f061d7
Brandon Cantello <bcantello@gmail.com>
2021-08-14 12:58:21 -0700

    still not working

--- src/services/calTechApiHelper.js
+++ src/services/calTechApiHelper.js
@@ -6,24 +6,15 @@ const parameters = {
 	order: 'koi_period',
 	format: 'json'
 };
-// let searchParameters = '';
 
 const api = axios.create({
 	baseURL: 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI'
 });
 
-// for (let element in parameters) {
-// 	if (element !== '') {
-// 		searchParameters += `?${parameters[element]}`;
-// 	}
-// }
-
-// console.log(searchParameters);
-
 export const getExoplanets = async () => {
 	try {
-		console.log(`${parameters}`);
-		return await api.get("", { parameters });
+		console.log(parameters);
+		return await api.get('', { parameters });
 		// return await api.get(`?table=cumulative&where=koi_disposition like 'CANDIDATE' and koi_period>300 and koi_prad<2&order=koi_period&format=json`);
 	} catch (error) {
 		return error;
