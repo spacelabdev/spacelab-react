68d32e01c9ebc63269b1f3a9afa7570b0775be64
Kausar Akther <kausar.akther@gmail.com>
2021-10-10 16:05:06 -0400

    Added history section lines

--- src/pages/about/history.js
+++ src/pages/about/history.js
@@ -13,6 +13,7 @@ export default function History(){
             <h2 className="about-history-header"> <span className="bold-history">History</span></h2>
             <div className="history-container">             
                 <img className={"about-history-image"} src={Planets_Circle} alt={"Space"}/>
+                <div className="history">
                 <div className="history-dot">
                     <img src={Dot} alt={"Space"}/>
                 </div>
@@ -25,11 +26,11 @@ export default function History(){
                         Risus Semper Faucibus.
                     </p>
                 </div>
-                <div className="about-history-content">  
-                    <h3>spacelab 2021.00.00</h3>  
+                <div className="about-history-content"> 
                     <div className="history-dot">
                         <img src={Dot} alt={"Space"}/>
-                    </div>              
+                     </div>  
+                    <h3>spacelab 2021.00.00</h3>               
                     <p>
                         Lerem Ipsum Dolor Sit Amet, Consectetur Adipiscing 
                         Elit Adipiscing Sed Risus Neque Faucibus Tempus Et 
@@ -37,17 +38,18 @@ export default function History(){
                         Risus Semper Faucibus.
                     </p>
                 </div>
-                <div className="about-history-content">  
-                    <h3>spacelab 2021.00.00</h3> 
+                <div className="about-history-content"> 
                     <div className="history-dot">
-                    <img src={Dot} alt={"Space"}/>
-                </div>               
+                        <img src={Dot} alt={"Space"}/>
+                    </div> 
+                    <h3>spacelab 2021.00.00</h3>                
                     <p>
                         Lerem Ipsum Dolor Sit Amet, Consectetur Adipiscing 
                         Elit Adipiscing Sed Risus Neque Faucibus Tempus Et 
                         Ante. Neque Aliquam Eleifend Donec Scelerisque Sagittis 
                         Risus Semper Faucibus.
                     </p>
+                    </div>
                 </div>
             </div>
         </section>
