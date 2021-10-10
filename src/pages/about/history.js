6c0d8260c831db1dd91f7b5ae4169500f14230b2
Kausar Akther <kausar.akther@gmail.com>
2021-10-09 22:21:30 -0400

    Added p tag

--- src/pages/about/history.js
+++ src/pages/about/history.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Planet_Rings from "../../assets/aboutAssets/planet_rings.svg"
+import Planets_Circle from '../../assets/aboutAssets/planets_circle.svg'
+import Dot from '../../assets/aboutAssets/dot.svg'
 
 /**
  * @returns {JSX.Element}
@@ -8,30 +9,46 @@ import Planet_Rings from "../../assets/aboutAssets/planet_rings.svg"
 
 export default function History(){
     return (
-        <section className="about-history-container">
-            <h1 className="about-history-header">HISTORY</h1>
-            <div className="about-history-image">   
-            <img className={"about-history-image"} src={Planet_Rings} alt={"Planet"}/>
-            </div>
-            <div className={"about-history-content"}>                   
-                <h3 className="history-paragraph-header">
-                    SPACELAB 2021.00.00
-                </h3>
-                <p>Lerem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit Adipiscing Sed Risus Neque Faucibus Tempus Et Ante. Neque Aliquam Eleifend Donec Scelerisque Sagittis Risus Semper Faucibus.</p>
-            </div>
-
-            <div className={"about-history-content"}>                   
-                <h3 className="history-paragraph-header">
-                    SPACELAB 2021.00.00
-                </h3>
-                <p>Lerem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit Adipiscing Sed Risus Neque Faucibus Tempus Et Ante. Neque Aliquam Eleifend Donec Scelerisque Sagittis Risus Semper Faucibus.</p>
-            </div>
-
-            <div className={"about-history-content"}>                   
-                <h3 className="history-paragraph-header">
-                    SPACELAB 2021.00.00
-                </h3>
-                <p>Lerem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit Adipiscing Sed Risus Neque Faucibus Tempus Et Ante. Neque Aliquam Eleifend Donec Scelerisque Sagittis Risus Semper Faucibus.</p>
+        <section className="history-section">
+            <h2 className="about-history-header"> <span className="bold-history">History</span></h2>
+            <div className="history-container">             
+                <img className={"about-history-image"} src={Planets_Circle} alt={"Space"}/>
+                <div className="history-dot">
+                    <img src={Dot} alt={"Space"}/>
+                </div>
+                <div className="about-history-content">  
+                    <h3>spacelab 2021.00.00</h3>                
+                    <p>
+                        Lerem Ipsum Dolor Sit Amet, Consectetur Adipiscing 
+                        Elit Adipiscing Sed Risus Neque Faucibus Tempus Et 
+                        Ante. Neque Aliquam Eleifend Donec Scelerisque Sagittis 
+                        Risus Semper Faucibus.
+                    </p>
+                </div>
+                <div className="about-history-content">  
+                    <h3>spacelab 2021.00.00</h3>  
+                    <div className="history-dot">
+                        <img src={Dot} alt={"Space"}/>
+                    </div>              
+                    <p>
+                        Lerem Ipsum Dolor Sit Amet, Consectetur Adipiscing 
+                        Elit Adipiscing Sed Risus Neque Faucibus Tempus Et 
+                        Ante. Neque Aliquam Eleifend Donec Scelerisque Sagittis 
+                        Risus Semper Faucibus.
+                    </p>
+                </div>
+                <div className="about-history-content">  
+                    <h3>spacelab 2021.00.00</h3> 
+                    <div className="history-dot">
+                    <img src={Dot} alt={"Space"}/>
+                </div>               
+                    <p>
+                        Lerem Ipsum Dolor Sit Amet, Consectetur Adipiscing 
+                        Elit Adipiscing Sed Risus Neque Faucibus Tempus Et 
+                        Ante. Neque Aliquam Eleifend Donec Scelerisque Sagittis 
+                        Risus Semper Faucibus.
+                    </p>
+                </div>
             </div>
         </section>
     )
