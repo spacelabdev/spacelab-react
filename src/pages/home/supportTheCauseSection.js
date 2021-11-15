bf79eef91d71d47d3e8db582879edcae409cbf44
aanand93 <77586218+aanand93@users.noreply.github.com>
2021-11-15 10:29:02 -0500

    Added text to support the cause section (donate)

--- src/pages/home/supportTheCauseSection.js
+++ src/pages/home/supportTheCauseSection.js
@@ -8,19 +8,28 @@ import NavigationButton from "../../components/navigationButton/navigationButton
  * @constructor
  */
 export default function SupportTheCauseSection() {
-	return (
-		<div id={"support-the-cause-section"}>
-			<div id={"support-the-cause-header"}>SUPPORT THE CAUSE</div>
-			<p id={"support-the-cause-content"}>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-				dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-				aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-				dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-				officia deserunt mollit anim id est laborum.
-			</p>
-			<div id={"contact-us-button-container"}>
-				<NavigationButton pathName={"/contact"} buttonText={"CONTACT US"}/>
-			</div>
-		</div>
-	);
-};
+    return (
+        <div id={"support-the-cause-section"}>
+            <div id={"support-the-cause-header"}>SUPPORT THE CAUSE</div>
+            <p id={"support-the-cause-content"}>
+                As diversity fuels creativity and innovation, we here at
+                SpaceLab strongly value the voices and perspectives of those
+                underrepresented in the community. One of our goals is to create
+                paid fellowships, internships, scholarship opportunities, and
+                debt relief for those who are underrepresented, including but
+                not limited to women, members of the LGBTQ+ community, and
+                people of color. Helping members of the community who struggle
+                economically will provide them with the liberty and flexibility
+                necessary to explore new career paths in science and technology.
+                To help us grow these opportunities, please consider making a
+                tax-deductible donation to SpaceLab today.
+            </p>
+            <div id={"contact-us-button-container"}>
+                <NavigationButton
+                    pathName={"/contact"}
+                    buttonText={"CONTACT US"}
+                />
+            </div>
+        </div>
+    );
+}
