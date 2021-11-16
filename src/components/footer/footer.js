f2b10f546094cf8b8a069ae178a4c68b5e77b116
aanand93 <77586218+aanand93@users.noreply.github.com>
2021-11-16 10:45:05 -0500

    Fixed line dimensions at bottom for desktop view

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -72,6 +72,9 @@ export default function Footer() {
             <div className="socialmedia-container">
                 <center>
                     <hr />
+                    {/* 
+										TODO: fill in the usernames for the social media links below
+										 */}
                     <IconContext.Provider
                         value={{ color: "D9D9D9", size: "2rem" }}
                     >
