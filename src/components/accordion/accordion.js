3a2843081b10e5fc5e2c54c5f2333f2bb351c87f
RohitGJ <77436246+rohitgj169@users.noreply.github.com>
2021-11-17 14:49:31 -0500

    setup accordion header

--- src/components/accordion/accordion.js
+++ src/components/accordion/accordion.js
@@ -1,10 +1,16 @@
-import React from 'react'
-import "./accordion.scss"
+import React from "react";
+import "./accordion.scss";
 
 export default function accordion() {
-  return (
-    <div className={"accordion-container"}>
-      Accordian
-    </div>
-  )
+    return (
+        <div className={"accordion-container"}>
+            <div className={"accordion-container-heading"}>
+                <div className={"accordion-container-heading-text"}>
+                    Board of Directors
+                </div>
+                <div className={"accordion-container-heading-icon"}>▲</div>
+            </div>
+            <div className={"accordion-container-content"}>Test</div>
+        </div>
+    );
 }
