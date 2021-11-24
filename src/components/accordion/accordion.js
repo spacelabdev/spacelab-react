c6e1c744cf91c6122141ea9f375e868ab7401b48
RohitGJ <77436246+rohitgj169@users.noreply.github.com>
2021-11-24 13:09:44 -0500

    convert to tabs

--- src/components/accordion/accordion.js
+++ src/components/accordion/accordion.js
@@ -10,52 +10,52 @@ import MemberCard from "../memberCard/memberCard";
  * @constructor
  */
 export default function Accordion(props) {
-    //States
-    const [open, setOpen] = useState(false);
-    const [arrow, setArrow] = useState(false);
-    //Props
-    const { title, members, firstLoad, setFirstLoad } = props;
+		//States
+		const [open, setOpen] = useState(false);
+		const [arrow, setArrow] = useState(false);
+		//Props
+		const { title, members, firstLoad, setFirstLoad } = props;
 
-    const handleAccordion = () => {
-        setOpen(!open);
-        setArrow(!arrow);
-        if (firstLoad) setFirstLoad(false);
-    };
+		const handleAccordion = () => {
+				setOpen(!open);
+				setArrow(!arrow);
+				if (firstLoad) setFirstLoad(false);
+		};
 
-    useEffect(() => {
-        if (firstLoad) {
-            setOpen(true);
-            setArrow(true);
-        }
-    }, [firstLoad]);
+		useEffect(() => {
+				if (firstLoad) {
+						setOpen(true);
+						setArrow(true);
+				}
+		}, [firstLoad]);
 
-    const openAccordion = open ? "accordion-open" : "";
-    let flipArrow = arrow ? "icon-flip" : "";
-    if (firstLoad) {
-        flipArrow = "icon-flip";
-    }
+		const openAccordion = open ? "accordion-open" : "";
+		let flipArrow = arrow ? "icon-flip" : "";
+		if (firstLoad) {
+				flipArrow = "icon-flip";
+		}
 
-    return (
-        <div className="accordion-container">
-            <div className="accordion-container-heading">
-                <div className="accordion-container-heading-text-container">
-                    {title}
-                </div>
-                <div
-                    className={`accordion-container-heading-icon ${flipArrow}`}
-                    onClick={handleAccordion}
-                >
-                    ▲
-                </div>
-            </div>
-            <div className={`accordion-container-content ${openAccordion}`}>
-                {members.map((member) => (
-                    <MemberCard
-                        key={`${title}-${member.fullName}`}
-                        member={member}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+		return (
+				<div className="accordion-container">
+						<div className="accordion-container-heading">
+								<div className="accordion-container-heading-text-container">
+										{title}
+								</div>
+								<div
+										className={`accordion-container-heading-icon ${flipArrow}`}
+										onClick={handleAccordion}
+								>
+										▲
+								</div>
+						</div>
+						<div className={`accordion-container-content ${openAccordion}`}>
+								{members.map((member) => (
+										<MemberCard
+												key={`${title}-${member.fullName}`}
+												member={member}
+										/>
+								))}
+						</div>
+				</div>
+		);
 }
