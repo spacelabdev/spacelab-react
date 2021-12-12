7c7ca88e880057c50767a60b643a256860f44ea2
Cat <morancathy@gmail.com>
2021-12-12 14:07:20 -0600

    update blogArchives page to include link to single BlogDetails page if user clicks on blog card

--- src/pages/blog/blogArchives/blogArchives.js
+++ src/pages/blog/blogArchives/blogArchives.js
@@ -2,6 +2,7 @@ import React, {useContext, useState} from "react";
 import {UniversalContext} from "../../../App";
 import HeroImage from "../../../components/heroImage/heroImage";
 import Footer from "../../../components/footer/footer";
+import BlogDetails from "../blogDetails/blogDetails";
 import "./blogArchives.scss";
 /**
  * Returns gallery of all blog posts
@@ -11,40 +12,14 @@ import "./blogArchives.scss";
 export default function BlogArchives() {
 	const context = useContext(UniversalContext);
 	const blogs = context.blogArray.items;
+	const searchForEmptyElement = context.searchForEmptyElement
+	const setBlogDetails = context.setBlogDetails
 	let blogArray = [];
-
-	// TODO: Not sure what you are trying to do with this?
 	const [open, setOpen] = useState(false)
-	const [article, setArticle] = useState([])
 
-	// TODO: Remove this array. It lives in the helperFunction
-	const emptyElements = [
-	  "<area",
-	  "<base",
-	  "<br",
-	  "<col",
-	  "<embed",
-	  "<hr",
-	  "<img",
-	  "<input",
-	  "<keygen",
-	  "<link",
-	  "<meta",
-	  "<param",
-	  "<source",
-	  "<track",
-	  "<wbr"
-	]
-
-	let strEnd = ""
-
-	// TODO: not sure what you are trying to do here. What you probably want to do, is have this function route
-	//  the user to a different page within Spacelab, and set the html string as state in App.js.
-	//  You could also potentially consider using session storage?
-	//  The new page would then call the parsing function from helperFunction and render the blog.
 	function handleClick(blog) {
 		setOpen(!open)
-		setArticle(blog)
+		setBlogDetails(blog)
 	}
 
 	if (blogs !== undefined) {
@@ -59,7 +34,6 @@ export default function BlogArchives() {
 
 			blogArray.push(
 				<a className={"blog-tile"}
-				   // href={blogs[i].link}
 				   onClick={() => handleClick(content)}
 				   target="_blank"
 				   rel="nofollow noopener noreferrer">
@@ -72,76 +46,23 @@ export default function BlogArchives() {
 							<p className={"blog-title"}>{blogs[i].title}</p>
 						</div>
 					</div>
-
 				</a>
-
 			);
 		}
 	}
 
-	// TODO: Remove these and call from the helper function where necessary.
-	/********************************************************************************
-	Fuction to search for emptyElement                                             */
-
-	function searchForEmptyElement(originalStr){
-	  let str = originalStr
-	  let updatedStr = ""
-	  let finalString = ""
-
-	   for(let i = 0; i < emptyElements.length; i++){
-	    let beforeElement = ""
-	    let afterElement = ""
-	    let result = emptyElements.some(w => str.includes(emptyElements[i]))
-	    let index = originalStr.indexOf(emptyElements[i])
-
-	    if(result === true){
-	      let foundElement = emptyElements[i]
-
-	      while (index !== -1) {
-	        let newIndex = str.indexOf(foundElement)
-	        afterElement =  str.substring(newIndex)
-	        beforeElement = str.substring(0, newIndex)
-	        afterElement = isTagClosed(afterElement)
-	        index = originalStr.indexOf(emptyElements[i], index + 1)
-	        updatedStr = updatedStr + beforeElement + afterElement
-	        str = strEnd
-	      }
-	      finalString = updatedStr + str
-	      str = finalString
-	      updatedStr = ""
-	    }
-	  }
-	  return finalString
-	}
-
-	/*	Fuction to determine if element is closed*/
-	function isTagClosed(elementStr){
-	  let [start, ...end] = elementStr.split('>');
-	  strEnd = end.join(">");
-
-	  if(start[start.length - 1] !== "/"){
-	    return `${start}/>`
-	  } else {
-	    return `${start}>`
-	  }
-	}
-	/********************************************************************************/
-
 	return (
 		<>
 			<HeroImage/>
+			{open &&
+				<BlogDetails />
+			}
 
 			{!open &&
 				<div id={"blog-archive-container"}>
 					<div id={"blog-archive-header"}>Archived Blogs</div>
 					<div id={"blog-archives"}>{blogArray}</div>
 				</div>
-			}
-
-			{ open &&
-				<>
-					{article}
-				</>
 			}
 			<Footer/>
 		</>
