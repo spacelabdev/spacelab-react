3501e12535e91c1f083851c549fe71f915ead010
Brandon Cantello <bcantello@gmail.com>
2021-12-14 08:57:27 -0800

    404 route error

--- src/main.js
+++ src/main.js
@@ -26,6 +26,7 @@ export default function Main() {
 			<Route exact path="/glossary" component={Glossary}/>
 			<Route exact path="/home" component={Home}/>
 			<Route exact path="/projects" component={Projects}/>
+			<Route path="/" component={Home}/>
 			<Redirect to='/home'/>
 		</Switch>
 	);
