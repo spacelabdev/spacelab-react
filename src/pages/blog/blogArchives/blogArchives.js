0f35e4d8a90a47c7c6f8b17ee77664f3915e704a
Cat <morancathy@gmail.com>
2021-12-13 19:23:56 -0600

    add route to blogDetails page

--- src/pages/blog/blogArchives/blogArchives.js
+++ src/pages/blog/blogArchives/blogArchives.js
@@ -1,9 +1,10 @@
-import React, {useContext, useState} from "react";
+import React, {useContext} from "react";
 import {UniversalContext} from "../../../App";
 import HeroImage from "../../../components/heroImage/heroImage";
 import Footer from "../../../components/footer/footer";
-import BlogDetails from "../blogDetails/blogDetails";
+import {Link} from "react-router-dom";
 import "./blogArchives.scss";
+
 /**
  * Returns gallery of all blog posts
  * @returns {JSX.Element}
@@ -12,41 +13,35 @@ import "./blogArchives.scss";
 export default function BlogArchives() {
 	const context = useContext(UniversalContext);
 	const blogs = context.blogArray.items;
-	const searchForEmptyElement = context.searchForEmptyElement
-	const setBlogDetails = context.setBlogDetails
+	const setBlogDetails = context.setBlogDetails;
 	let blogArray = [];
-	const [open, setOpen] = useState(false)
 
 	function handleClick(blog) {
-		setOpen(!open)
-		setBlogDetails(blog)
+		setBlogDetails(blog);
 	}
 
 	if (blogs !== undefined) {
 		for (let i = 0; i < blogs.length; i++) {
 			let content = blogs[i]['content:encoded'];
 			content = content.toString();
-
 			let imageURL = content.substring(content.indexOf("src=") + 5,
 				content.indexOf(">", content.indexOf("src=") + 5) - 3);
-
-			content = searchForEmptyElement(content)
-
 			blogArray.push(
-				<a className={"blog-tile"}
-				   onClick={() => handleClick(content)}
-				   target="_blank"
-				   rel="nofollow noopener noreferrer">
-					<div className={"blog-tile-image-wrapper"}>
-						<img className={"blog-tile-image"}
-						     src={imageURL}
-						     alt={"blog tile"}
-						/>
-						<div>
-							<p className={"blog-title"}>{blogs[i].title}</p>
+				<Link to={"/blogdetails"} className={"blog-tile"}>
+					<a
+					   onClick={() => handleClick(content)}		
+					   rel="nofollow noopener noreferrer">
+						<div className={"blog-tile-image-wrapper"}>
+							<img className={"blog-tile-image"}
+							     src={imageURL}
+							     alt={"blog tile"}
+							/>
+							<div>
+								<p className={"blog-title"}>{blogs[i].title}</p>
+							</div>
 						</div>
-					</div>
-				</a>
+					</a>
+				</Link>
 			);
 		}
 	}
@@ -54,16 +49,10 @@ export default function BlogArchives() {
 	return (
 		<>
 			<HeroImage/>
-			{open &&
-				<BlogDetails />
-			}
-
-			{!open &&
-				<div id={"blog-archive-container"}>
-					<div id={"blog-archive-header"}>Archived Blogs</div>
-					<div id={"blog-archives"}>{blogArray}</div>
-				</div>
-			}
+			<div id={"blog-archive-container"}>
+				<div id={"blog-archive-header"}>Archived Blogs</div>
+				<div id={"blog-archives"}>{blogArray}</div>
+			</div>
 			<Footer/>
 		</>
 	);
