9d6af5a384debfd9bdf5f95c960975a3a46f3fc1
sven-gerlach <svengerlach@me.com>
2021-12-17 00:27:24 +0100

    Resolve linter warnings
    1) In discovery.js the useEffect(queryExoplanetDatabase, []) related
    linter warning has been manually turned off. Since providing an empty
    dependency array is done intentionally. The callback is only invoked
    once at component mount time. Providing the callback's dependencies
    would execute the callback function every time the dependencies
    change - an effect that is not desired in this case.
    
    2) In discovery.js the switch control statement has been supplemented
    with an empty default case. Whilst not strictly necessary since the two
    cases (csv, json) provided are exhaustive, it does suffice eslint's
    requirement for a default case.
    
    3) In utilityFunctions.js the useEffectSkipFirstRender custom hook's
    related linter warning has been turned off manually. The function
    argument never changes at run-time and the arr argument type is array.

--- src/pages/discovery/discovery.js
+++ src/pages/discovery/discovery.js
@@ -1,16 +1,114 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
 import HeroImage from "../../components/heroImage/heroImage";
-import DiscoveryFilterList from "./discoverySearchFilters/discoveryFilterList";
-import {discoveryMethodFiltersArray, planetSystemsFiltersArray, planetTypeFiltersArray} from "./discoveryHelper";
-import "./discovery.scss";
+import DiscoveryColumnFilterList from "./discoverySearchFilters/discoveryColumnFilterList";
+import {
+	projectDispositionFiltersArray,
+	identificationFiltersArray,
+	exoplanetArchiveFiltersArray,
+	transitPropertiesFiltersArray,
+	thresholdCrossingEventFiltersArray,
+	stellarParametersFiltersArray,
+	kicParametersFiltersArray,
+	pixelBasedKoiVettingFiltersArray,
+} from "./discoveryHelper";
 import Footer from "../../components/footer/footer";
 import UnderConstruction from "../../components/underConstructionNotification/underConstruction";
+import {getExoplanets} from "../../services/calTechApiRequest";
+import {UniversalContext} from "../../App";
+import {initialiseSelectedColumnsState, initialiseWhereFilterState} from "./initialiseState";
+import DropdownButton from "../../components/button/dropdownButton";
+import {downloadData} from "../../services/utilityFunctions";
+import "./discovery.scss";
 
 /**
- * @returns {JSX.Element}
+ * View for the discovery page
+ * @return {JSX.Element}
  * @constructor
  */
 export default function Discovery() {
+	const context = useContext(UniversalContext);
+	// todo: [Sven Gerlach] given that API returns are saved in session storage it would make sense to store and set
+	//  state for the checkboxes in App instead
+	const [selectedColumns, setSelectedColumns] = useState(initialiseSelectedColumnsState());
+	const [whereFilter, setWhereFilter] = useState(initialiseWhereFilterState());
+
+	/**
+	 * Query CalTech db and set exoplanetData in App state and store as session var. If a query is sent for the purpose
+	 * of downloading the result, the signature of this function allows the provision of an alternative setState
+	 * function to ensure that the state for the display of the filtered data (JSON) is not mixed with data responses
+	 * for the purpose of downloading filtered results (likely in csv format)
+	 * @param format: desired format of API response (empty string -> csv)
+	 * @param isStateful: if false, do not set state (needed for download feature)
+	 * @param isStorage: if false, API response not saved in storage (need for download feature)
+	 */
+	const queryExoplanetDatabase = (format = 'json', isStateful = true, isStorage = true) => {
+		// only send an API request if at least one column has been checked
+		if (Object.values(selectedColumns).some(column => column)) {
+			return getExoplanets({
+				select: selectedColumns,
+				where: whereFilter,
+				order: '',
+				format: format,
+			}).then(res => {
+				if (res.status === 200) {
+					if (isStateful) {
+						context.setExoplanetData(res.data);
+					}
+					/*  todo: why store this in session storage at all? */
+					if (isStorage) {
+						sessionStorage.setItem('exoplanetSearchResults', JSON.stringify(res.data));
+					}
+				} else {
+					console.error("error retrieving exoplanetData");
+				}
+				// return response such that it can be handled by the caller
+				// this is important for the data download function
+				return res;
+			}).catch(e => console.error(e));
+		}
+	};
+
+	// Make API call after selectedColumns and whereFilter states have been initialised but only once at component
+	// mount-time. Disabling the eslint warning in the next line since providing an empty dependency array is done
+	// intentionally. The callback is only invoked once at component mount time. Providing the callback's
+	// dependencies would execute the callback function everytime the dependencies change - an effect that is not
+	// desired in this case. The next line disables the eslint alert.
+	// eslint-disable-next-line
+	useEffect(queryExoplanetDatabase, []);
+
+	/**
+	 * This function is passed as a prop to the dropdown item and is executed when the user clicks on the item
+	 */
+	const dropdownItemClick = (e) => {
+		// set format, dataType, and filename, subject to selected data type
+		let format;
+		let dataType;
+		let filename;
+		switch (e.target.innerHTML) {
+			case 'csv':
+				// as per API, empty string for format requires a csv data response
+				format = '';
+				dataType = 'text/csv';
+				filename = 'filtered_output.csv';
+				break;
+			case 'json':
+				format = 'json';
+				dataType = 'application/json';
+				filename = 'filtered_output.json';
+				break;
+			// Default case provided only to suffice eslint's requirement for a default case
+			default:
+		}
+
+		// make API request with specified data type format (Note: empty string = csv)
+		queryExoplanetDatabase(format, false, false)
+			.then(res => {
+				// stringify data only if format is json
+				const data = format === 'json' ? JSON.stringify(res.data) : res.data;
+				downloadData(data, dataType, filename);
+			})
+			.catch(e => console.error(e));
+	}
 
 	return (
 		<>
@@ -28,10 +126,88 @@ export default function Discovery() {
 				<br/>
 
 				<div id={"filtersContainer"}>
-					<p>Filters</p>
-					<DiscoveryFilterList filterArray={planetSystemsFiltersArray} title={"Planet Systems"}/>
-					<DiscoveryFilterList filterArray={planetTypeFiltersArray} title={"Planet Type"}/>
-					<DiscoveryFilterList filterArray={discoveryMethodFiltersArray} title={"Discovery Method"}/>
+					<div id={"filtersHeader"}>
+						<p>Filters</p>
+						<DropdownButton
+							buttonLabel={'Download'}
+							queryExoplanetDatabase={queryExoplanetDatabase}
+							dropdownItemClick={dropdownItemClick}
+							item1={{href: "#/action-1", label: "csv"}}
+							item2={{href: "#/action-2", label: "json"}}
+						/>
+					</div>
+					<DiscoveryColumnFilterList
+						filterArray={identificationFiltersArray}
+						title={"Identifications"}
+						selectedColumns={selectedColumns}
+						setSelectedColumns={setSelectedColumns}
+						whereFilter={whereFilter}
+						setWhereFilter={setWhereFilter}
+						queryExoplanetDatabse={queryExoplanetDatabase}
+					/>
+					<DiscoveryColumnFilterList
+						filterArray={exoplanetArchiveFiltersArray}
+						title={"Exoplanets"}
+						selectedColumns={selectedColumns}
+						setSelectedColumns={setSelectedColumns}
+						whereFilter={whereFilter}
+						setWhereFilter={setWhereFilter}
+						queryExoplanetDatabse={queryExoplanetDatabase}
+					/>
+					<DiscoveryColumnFilterList
+						filterArray={projectDispositionFiltersArray}
+						title={"Dispositions"}
+						selectedColumns={selectedColumns}
+						setSelectedColumns={setSelectedColumns}
+						whereFilter={whereFilter}
+						setWhereFilter={setWhereFilter}
+						queryExoplanetDatabse={queryExoplanetDatabase}
+					/>
+					<DiscoveryColumnFilterList
+						filterArray={transitPropertiesFiltersArray}
+						title={"Transit Properties"}
+						selectedColumns={selectedColumns}
+						setSelectedColumns={setSelectedColumns}
+						whereFilter={whereFilter}
+						setWhereFilter={setWhereFilter}
+						queryExoplanetDatabse={queryExoplanetDatabase}
+					/>
+					<DiscoveryColumnFilterList
+						filterArray={thresholdCrossingEventFiltersArray}
+						title={"Threshold Crossing Events"}
+						selectedColumns={selectedColumns}
+						setSelectedColumns={setSelectedColumns}
+						whereFilter={whereFilter}
+						setWhereFilter={setWhereFilter}
+						queryExoplanetDatabse={queryExoplanetDatabase}
+					/>
+					<DiscoveryColumnFilterList
+						filterArray={stellarParametersFiltersArray}
+						title={"Stellar Parameters"}
+						selectedColumns={selectedColumns}
+						setSelectedColumns={setSelectedColumns}
+						whereFilter={whereFilter}
+						setWhereFilter={setWhereFilter}
+						queryExoplanetDatabse={queryExoplanetDatabase}
+					/>
+					<DiscoveryColumnFilterList
+						filterArray={kicParametersFiltersArray}
+						title={"KIC Parameters"}
+						selectedColumns={selectedColumns}
+						setSelectedColumns={setSelectedColumns}
+						whereFilter={whereFilter}
+						setWhereFilter={setWhereFilter}
+						queryExoplanetDatabse={queryExoplanetDatabase}
+					/>
+					<DiscoveryColumnFilterList
+						filterArray={pixelBasedKoiVettingFiltersArray}
+						title={"Pixel Based KOI Vetting"}
+						selectedColumns={selectedColumns}
+						setSelectedColumns={setSelectedColumns}
+						whereFilter={whereFilter}
+						setWhereFilter={setWhereFilter}
+						queryExoplanetDatabse={queryExoplanetDatabase}
+					/>
 				</div>
 			</div>
 			<Footer/>
