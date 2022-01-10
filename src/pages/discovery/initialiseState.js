bbd1cb69927c1d66093025a3b85bd661c81f1b66
sven-gerlach <svengerlach@me.com>
2022-01-10 12:22:16 -0500

    Session-store selected columns and filters
    Previously, the data response from the API request was stored in
    session storage. This did not work well since there were configurations
    for which the returned data was too large for browser's session storage
    to handle / save it. Also, retrieving the data from the API is cheap and
    fast but users spend most time with fine-tuning the column selection
    and the filter options.
    
    As a result, the response data from the API is not stored in session
    storage anymore. Instead, the application now stores the selectedColumns
    and whereFilter objects in session storage.
    
    The simpleButton component has also been amended to such effect that a
    single click on the button does not execute the provided callback more
    than once. This was achieved by removing the buttonEffectAsync callback
    from the dependency list inside the useEffect hook.

--- src/pages/discovery/initialiseState.js
+++ src/pages/discovery/initialiseState.js
@@ -8,13 +8,20 @@ import * as filterArrays from "./discoveryHelper";
  * This object will initialise the checked state for the checkboxes.
  */
 export function initialiseSelectedColumnsState() {
-	const stateObject = {};
+	let stateObject = {};
 
-	for (let filterArray in filterArrays) {
-		filterArray = filterArrays[filterArray];
-		for (let filter in filterArray) {
-			filter = filterArray[filter];
-			stateObject[filter.name] = filter.default
+	// if selectedColumns object stored in session data use this, else use filterArrays from discoveryHelper module
+	if (sessionStorage.getItem("selectedColumns")) {
+		const selectedColumnsJSON = sessionStorage.getItem("selectedColumns")
+		stateObject = JSON.parse(selectedColumnsJSON)
+	}
+	else {
+		for (let filterArray in filterArrays) {
+			filterArray = filterArrays[filterArray];
+			for (let filter in filterArray) {
+				filter = filterArray[filter];
+				stateObject[filter.name] = filter.default
+			}
 		}
 	}
 
@@ -22,23 +29,30 @@ export function initialiseSelectedColumnsState() {
 }
 
 export function initialiseWhereFilterState() {
-	const stateObject = {};
+	let stateObject = {};
 
-	for (let filterArray in filterArrays) {
-		filterArray = filterArrays[filterArray];
-		for (let filter in filterArray) {
-			filter = filterArray[filter];
-			if (filter.default) {
-				switch (filter.dataType) {
-					case 'number':
-					case 'date':
-						stateObject[filter.name] = {'operator': '<', 'value': '', 'dataType': filter.dataType};
-						break;
-					case 'string':
-					case 'enum':
-						stateObject[filter.name] = {'value': '', 'dataType': filter.dataType};
-						break;
-					default:
+	// use session data if whereFilter key stored in session storage, else use filterArrays from discoveryHelper module
+	if (sessionStorage.getItem("whereFilter")) {
+		const whereFilterJSON = sessionStorage.getItem("whereFilter")
+		stateObject = JSON.parse(whereFilterJSON)
+	}
+	else {
+		for (let filterArray in filterArrays) {
+			filterArray = filterArrays[filterArray];
+			for (let filter in filterArray) {
+				filter = filterArray[filter];
+				if (filter.default) {
+					switch (filter.dataType) {
+						case 'number':
+						case 'date':
+							stateObject[filter.name] = {'operator': '<', 'value': '', 'dataType': filter.dataType};
+							break;
+						case 'string':
+						case 'enum':
+							stateObject[filter.name] = {'value': '', 'dataType': filter.dataType};
+							break;
+						default:
+					}
 				}
 			}
 		}
