5035c0455f2dbb860c5ed903f6f907ad2ea218ff
aanand93 <77586218+aanand93@users.noreply.github.com>
2022-01-21 15:53:00 -0500

    Finished dynamic search bar.  Filters through the search terms and can be closed if the 'x' is clicked.  TODO: 1) add an onClick to the filtered search term once something is entered. 2) center the filtered listonce input is populated

--- src/pages/glossary/SearchBar.js
+++ src/pages/glossary/SearchBar.js
@@ -1,21 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Searchbar.css";
 import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from "@mui/icons-material/Close";
 
 function SearchBar({ placeholder, data }) {
+	const [filteredData, setFilteredData] = useState([]);
+	const [enteredWord, setEnteredWord] = useState("");
+
+	/*Filtering the searches when something is typed in the input */
+	const handleFilter = (event) => {
+		const searchWord = event.target.value;
+		setEnteredWord(searchWord);
+		const newFilter = data.filter((value) => {
+			return value[0].toLowerCase().includes(searchWord.toLowerCase());
+		});
+		/* if something is typed into the input, show the new filtered list */
+		if (searchWord === "") {
+			setFilteredData([]);
+		} else {
+			setFilteredData(newFilter);
+		}
+	};
+	/* Clearing the input when function is called onClick of the close icon */
+	const clearInput = () => {
+		setFilteredData([]);
+		setEnteredWord("");
+	};
+
 	return (
 		<div className="search-bar">
 			<div className="searchInputs">
-				<input type="text" placeholder={placeholder} />
+				<input
+					type="text"
+					placeholder={placeholder}
+					value={enteredWord}
+					onChange={handleFilter}
+				/>
 				<div className="searchIcon">
-					<SearchIcon />
+					{filteredData.length === 0 ? (
+						<SearchIcon />
+					) : (
+						<CloseIcon id="clearBtn" onClick={clearInput} />
+					)}
 				</div>
 			</div>
-			<div className="dataResult">
-				{/* {data.map((value, key) => {
-					return <div>{value.glossaryTermArray}</div>;
-				})} */}
-			</div>
+			{filteredData.length !== 0 && (
+				<div className="dataResult">
+					{/* just the search term (idex position 0) is mapped, not the definition. Can change this to definitions as well eventually */}
+					{filteredData.slice(0, 10).map((value, key) => {
+						return (
+							<div className="dataItem">
+								<p>{value[0]}</p>
+							</div>
+						);
+					})}
+				</div>
+			)}
 		</div>
 	);
 }
