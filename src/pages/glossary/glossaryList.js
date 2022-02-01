aa1ecbe78dee201bd69d33e61e82737726c88f4b
aanand93 <77586218+aanand93@users.noreply.github.com>
2022-02-01 10:15:56 -0500

    Rebase

--- src/pages/glossary/glossaryList.js
+++ src/pages/glossary/glossaryList.js
@@ -1,9 +1,14 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import GlossaryListItem from "./glossaryListItem";
-import {UniversalContext} from "../../App";
-import {returnFilteredTerms, handleTermImage, handleMissingTermImage, highlightCurrentFilter} from "./glossaryhelper";
+import { UniversalContext } from "../../App";
+import {
+	returnFilteredTerms,
+	handleTermImage,
+	handleMissingTermImage,
+	highlightCurrentFilter,
+} from "./glossaryhelper";
 import placeholderImage from "../../assets/generalAssets/img_placeholder.png";
-import './glossary.scss';
+import "./glossary.scss";
 
 export default function GlossaryList() {
 	const context = useContext(UniversalContext);
@@ -32,54 +37,67 @@ export default function GlossaryList() {
 	}
 
 	// Updates displayed terms based on selected filter
-	const handleFilterClick = (beginningAlphabetIndex, endingAlphabetIndex, filterRange) => {
-		const termsGroup = returnFilteredTerms(beginningAlphabetIndex, endingAlphabetIndex);
-		const currentSelection = 'glossary-filter-button-' + filterRange;
+	const handleFilterClick = (
+		beginningAlphabetIndex,
+		endingAlphabetIndex,
+		filterRange
+	) => {
+		const termsGroup = returnFilteredTerms(
+			beginningAlphabetIndex,
+			endingAlphabetIndex
+		);
+		const currentSelection = "glossary-filter-button-" + filterRange;
 		context.setGlossaryTerms(termsGroup);
 		highlightCurrentFilter(currentSelection);
 	};
 
 	// map over each element in the glossaryTermsArray and pass them to the GlossaryListItem component as props
 	const glossaryTermArray = glossaryArray.map((e, index) => {
-		return (
-			<GlossaryListItem element={e} key={index}/>
-		);
+		return <GlossaryListItem element={e} key={index} />;
 	});
 
 	return (
-		<div className={'glossary-results-wrapper'}>
-			<div id={'glossary-button-container'}>
+		<div className={"glossary-results-wrapper"}>
+			<div id={"glossary-button-container"}>
 				<span
-					className={'glossary-filter-button'}
-					id={'glossary-filter-button-A-J'}
-					onClick={() => handleFilterClick(1, 9, 'A-J')}>
+					className={"glossary-filter-button"}
+					id={"glossary-filter-button-A-J"}
+					onClick={() => handleFilterClick(1, 9, "A-J")}
+				>
 					A - J
 				</span>
 				<span
-					className={'glossary-filter-button'}
-					id={'glossary-filter-button-K-S'}
-					onClick={() => handleFilterClick(10, 19, 'K-S')}>
+					className={"glossary-filter-button"}
+					id={"glossary-filter-button-K-S"}
+					onClick={() => handleFilterClick(10, 19, "K-S")}
+				>
 					K - S
 				</span>
 				<span
-					className={'glossary-filter-button'}
-					id={'glossary-filter-button-T-Z'}
-					onClick={() => handleFilterClick(20, 26, 'T-Z')}>
+					className={"glossary-filter-button"}
+					id={"glossary-filter-button-T-Z"}
+					onClick={() => handleFilterClick(20, 26, "T-Z")}
+				>
 					T - Z
 				</span>
 			</div>
-			<div id={'glossary-term-wrapper'}>{glossaryTermArray}</div>
-			<div id={'glossary-term-name'}>{context.currentGlossaryTerm}</div>
-			<div id={'glossary-term-def'}>
+			<div id={"glossary-term-wrapper"}>{glossaryTermArray}</div>
+			<div id={"glossary-term-name"}>{context.currentGlossaryTerm}</div>
+			<div id={"glossary-term-def"}>
 				{context.glossaryTermDef}
-				<div id={'glossary-term-image-wrapper'}>
-					<img id={'glossary-term-image'} src={termImage} alt={`${context.currentGlossaryTerm}`}/>
+				<div id={"glossary-term-image-wrapper"}>
+					<img
+						id={"glossary-term-image"}
+						src={termImage}
+						alt={`${context.currentGlossaryTerm}`}
+					/>
 				</div>
-				<div id={'glossary-term-image-source-wrapper'}>
-					<a id={'glossary-term-image-source'}
-					   href={termImageSource}
-					   target="_blank"
-					   rel="nofollow noopener noreferrer"
+				<div id={"glossary-term-image-source-wrapper"}>
+					<a
+						id={"glossary-term-image-source"}
+						href={termImageSource}
+						target="_blank"
+						rel="nofollow noopener noreferrer"
 					>
 						{termImageSource}
 					</a>
@@ -87,4 +105,4 @@ export default function GlossaryList() {
 			</div>
 		</div>
 	);
-};
+}
