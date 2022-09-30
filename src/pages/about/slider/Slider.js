915810c17e9ba8db22090e1ba7a32782c35bfc51
Alexander Anand <77586218+aanand93@users.noreply.github.com>
2022-09-30 12:22:25 -0400

    added left and right buttons to slider, added dots at bottom. Dots can be clicked to change slides as well.

--- src/pages/about/slider/Slider.js
+++ src/pages/about/slider/Slider.js
@@ -2,17 +2,36 @@ import React, { useState } from "react";
 import "./Slider.scss";
 import dataSlider from "./dataSlider";
 import Menu from "./Menu";
+import BtnSlider from "./BtnSlider";
 
 export default function Slider() {
 	const [slideIndex, setSlideIndex] = useState(1);
 
+    const nextSlide = () => {
+		if (slideIndex !== dataSlider.length) {
+			setSlideIndex(slideIndex + 1);
+		} else if (slideIndex === dataSlider.length) {
+			setSlideIndex(1);
+		}
+	};
+
+	const prevSlide = () => {
+		if (slideIndex !== 1) {
+			setSlideIndex(slideIndex - 1);
+		} else if (slideIndex === 1) {
+			setSlideIndex(dataSlider.length);
+		}
+	};
+
+	const moveDot = (index) => {
+		setSlideIndex(index);
+	};
+
 	return (
 		<div className="container-slider">
 			<div className="header">
-                <div className="meet-our-team">
-                    Meet Our Team
-                </div>
-            </div>
+				<div className="meet-our-team">Meet Our Team</div>
+			</div>
 			<Menu setSlideIndex={setSlideIndex} />
 			{dataSlider.map((obj, index) => {
 				return (
@@ -37,6 +56,20 @@ export default function Slider() {
 					</div>
 				);
 			})}
+
+			<BtnSlider moveSlide={nextSlide} direction={"next"} />
+			<BtnSlider moveSlide={prevSlide} direction={"prev"} />
+
+			<div className="container-dots">
+				{Array.from({ length: 5 }).map((item, index) => (
+					<div
+						onClick={() => moveDot(index + 1)}
+						className={
+							slideIndex === index + 1 ? "dot active" : "dot"
+						}
+					></div>
+				))}
+			</div>
 		</div>
 	);
 }
