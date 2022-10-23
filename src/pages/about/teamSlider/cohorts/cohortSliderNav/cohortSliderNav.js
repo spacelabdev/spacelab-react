7bbb305e8b03dfed20617a892e87635b5772ec8a
Brandon Cantello <bcantello@gmail.com>
2022-10-23 10:25:19 -0700

    styling

--- src/pages/about/teamSlider/cohorts/cohortSliderNav/cohortSliderNav.js
+++ src/pages/about/teamSlider/cohorts/cohortSliderNav/cohortSliderNav.js
@@ -5,10 +5,15 @@ export default function CohortSliderNav({activeCohort, setActive}) {
 	return (
 		< div className={"menu"}>
 			<div className={"cohorts-slider-nav-button-container"}>
-				<div className={`${activeCohort === 'december2021' ? 'active' : ''}`}
-				     onClick={() => setActive('december2021')}
+				<div className={`${activeCohort === 'december2021' ? 'active' : 'inactive'}`}
+				   onClick={() => setActive('december2021')}
 				>
 					Dec 2021
+				</div>
+				<div className={`${activeCohort === 'additionalContributors' ? 'active' : 'inactive'}`}
+				      onClick={() => setActive('additionalContributors')}
+				>
+					Additional Contributors
 				</div>
 			</div>
 		</div>
--- src/pages/about/teamSlider/cohorts/cohortsSlider/cohortsSlider.js
+++ src/pages/about/teamSlider/cohorts/cohortsSlider/cohortsSlider.js
@@ -2,12 +2,13 @@ import React, {useState} from "react";
 import CohortSliderNav from "../cohortSliderNav/cohortSliderNav";
 import SliderComponent from "../../SliderComponent/sliderComponent";
 import "./cohortsSlider.scss";
-import {december2021Cohort} from "../../../aboutHelper";
+import {december2021Cohort, additionalContributors} from "../../../aboutHelper";
 
 export default function CohortsSlider({toggle}) {
 	const [activeCohort, setActiveCohort] = useState('december2021');
 
-	const {title: december2021Title, members: december2021Members} = december2021Cohort
+	const {title: december2021Title, members: december2021Members} = december2021Cohort;
+	const {title: additionalContributorsTitle, members: additionalContributorMembers} = additionalContributors;
 
 	return (
 		<div id={'cohort-component-container'} className={toggle}>
@@ -15,6 +16,10 @@ export default function CohortsSlider({toggle}) {
 			<SliderComponent active={`${activeCohort === 'december2021' ? '' : 'hidden'}`}
 			                 title={december2021Title}
 			                 members={december2021Members}
+			/>
+			<SliderComponent active={`${activeCohort === 'additionalContributors' ? '' : 'hidden'}`}
+			                 title={additionalContributorsTitle}
+			                 members={additionalContributorMembers}
 			/>
 		</div>
 	);
