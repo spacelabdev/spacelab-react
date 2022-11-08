c980304e7df85465ea6fbe6def8dcf9a7f62ea7c
Mark <58246556+DrDrewCain@users.noreply.github.com>
2022-11-07 23:25:17 -0500

    Remove Button

--- src/pages/about/aboutTeamSliderSection.js
+++ src/pages/about/aboutTeamSliderSection.js
@@ -21,16 +21,6 @@ export default function AboutTeamSliderSection() {
 				<h2 className="meet-our-team">Meet Our Team</h2>
 			</div>
 			<div className="about-team-toggle">
-				<button id={'currentTeam'}
-				        className={`toggle-button ${activeComponents[0] === 'currentTeam' ? 'active' : 'inactive'}`}
-				        onClick={() => teamToggle('currentTeam')}
-				>Current Team
-				</button>
-				<button id={'pastCohorts'}
-				        className={`toggle-button ${activeComponents[0] === 'pastCohorts' ? 'active' : 'inactive'}`}
-				        onClick={() => teamToggle('pastCohorts')}
-				>Past Cohorts
-				</button>
 			</div>
 			<CurrentTeamSlider id={'currentTeamSlider'}
 			                   toggle={`${activeComponents[1] === 'currentTeamSlider' ? '' : 'hidden'}`}
