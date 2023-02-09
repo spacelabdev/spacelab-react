e614e425cc0342ca829e28f2834190bcfeea5778
Laura Hiller <Hillerelaura@gmail.com>
2023-02-08 20:06:17 -0800

    Added under construction image as placeholder for podcast page

--- src/pages/podcast/podcast.js
+++ src/pages/podcast/podcast.js
@@ -3,6 +3,7 @@ import HeroImage from "../../components/heroImage/heroImage";
 import Footer from "../../components/footer/footer";
 import {podcastArray} from "./podcastHelper";
 import "./podcast.scss";
+import UnderConstruction from "../../components/underConstructionNotification/underConstruction";
 
 /**
  * Renders Blog page
@@ -13,6 +14,7 @@ export default function Podcast() {
 	return (
 		<>
 			<HeroImage heroTitle="PODCAST" />
+			<UnderConstruction />
 			<Footer />
 		</>
 	);
