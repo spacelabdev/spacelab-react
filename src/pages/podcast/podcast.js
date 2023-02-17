7d59a166b0ce5b08e3988f6e4aee5b635828b442
Eric Matlock <ermatlock@gmail.com>
2023-02-16 21:48:53 -0500

    Map podcast cards

--- src/pages/podcast/podcast.js
+++ src/pages/podcast/podcast.js
@@ -1,7 +1,7 @@
 import React from "react";
 import HeroImage from "../../components/heroImage/heroImage";
 import Footer from "../../components/footer/footer";
-import {podcastArray} from "./podcastHelper";
+import { podcastArray } from "./podcastHelper";
 import "./podcast.scss";
 import PodcastCard from "./podcastCard";
 import UnderConstruction from "../../components/underConstructionNotification/underConstruction";
@@ -12,11 +12,30 @@ import UnderConstruction from "../../components/underConstructionNotification/un
  * @constructor
  */
 export default function Podcast() {
+	console.log("🚀 ~ file: podcast.js:5 ~ podcastArray", podcastArray);
 	return (
 		<>
 			<HeroImage heroTitle="PODCAST" />
-			<UnderConstruction/>
-			<PodcastCard/>
+			{/* <UnderConstruction /> */}
+			{podcastArray.map((item) => {
+				console.log("image", item.image);
+				return (
+					<PodcastCard
+						key={`ep${item.episodeNumber}`}
+						image={item.image}
+						title={item.title}
+						episodeNumber={item.episodeNumber}
+						seasonNumber={item.seasonNumber}
+						tags={item.tags}
+						description={item.description}
+						datePosted={item.datePosted}
+						duration={item.duration}
+						youtubeLink={item.youtubeLink}
+						spotifyLink={item.spotifyLink}
+					/>
+				);
+			})}
+
 			<Footer />
 		</>
 	);
