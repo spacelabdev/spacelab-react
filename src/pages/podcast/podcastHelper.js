1398178247ef411939292d25f8c9a2b7c5e56017
Eric Matlock <ermatlock@gmail.com>
2023-02-16 21:47:40 -0500

    Update helper data

--- src/pages/podcast/podcastHelper.js
+++ src/pages/podcast/podcastHelper.js
@@ -1,43 +1,56 @@
-import Construction from "../../assets/componentAssets/UnderConstruction.svg";
+import tempImage from "../../../src/assets/ju-guan-D-jLxBtEwaA-unsplash.jpg";
+
 /**
  * Array of Dates and descriptions for Spacelab History timeline
  * @type {string[][]}
  */
 export const podcastArray = [
-	[
-        {
-            image: Construction,
-            title: 'Title Episode',
-            episodeNumber: 0,
-            tags: ["Planets","Space","Stars"],
-            description: '',
-            datePosted: '02/08/2023',
-            duration: '1 hour and 3 min',
-            youtubeLink: 'https://www.youtube.com/',
-            spotifyLink: 'https://open.spotify.com/'
-        },
-        {
-            image: Construction,
-            title: 'Title Episode',
-            episodeNumber: 1,
-            tags: ["Asteroids","Moon","Earth"],
-            description: '',
-            datePosted: '02/08/2023',
-            duration: '1 hour and 3 min',
-            youtubeLink: 'https://www.youtube.com/',
-            spotifyLink: 'https://open.spotify.com/'
-        },
-        {
-            image: Construction,
-            title: 'Title Episode',
-            episodeNumber: 2,
-            tags: ["Supernova","Jupiter","Astronaut"],
-            description: '',
-            datePosted: '02/08/2023',
-            duration: '1 hour and 3 min',
-            youtubeLink: 'https://www.youtube.com/',
-            spotifyLink: 'https://open.spotify.com/'
-        }
-	
-    ]
+	{
+		image: tempImage,
+		title: "Title Episode",
+		episodeNumber: 1,
+		seasonNumber: 1,
+		tags: ["Planets", "Space", "Stars"],
+		description: `ipsum dolor sit amet, consectetur adipiscing elit. Urna,
+            imperdiet adipiscing libero commodo egestas ac pretium, at.
+            Viverra viverra quam non aliquet. Nec volutpat in morbi
+            molestie sem porttitor massa. Massa ac viverra montes,
+            maecenas tempus, lorem ultrices.`,
+		datePosted: "02/08/2023",
+		duration: "1 hour and 3 min",
+		youtubeLink: "https://www.youtube.com/",
+		spotifyLink: "https://open.spotify.com/",
+	},
+	{
+		image: tempImage,
+		title: "Title Episode",
+		episodeNumber: 2,
+		seasonNumber: 1,
+		tags: ["Asteroids", "Moon", "Earth"],
+		description: `ipsum dolor sit amet, consectetur adipiscing elit. Urna,
+            imperdiet adipiscing libero commodo egestas ac pretium, at.
+            Viverra viverra quam non aliquet. Nec volutpat in morbi
+            molestie sem porttitor massa. Massa ac viverra montes,
+            maecenas tempus, lorem ultrices.`,
+		datePosted: "02/08/2023",
+		duration: "1 hour and 3 min",
+		youtubeLink: "https://www.youtube.com/",
+		spotifyLink: "https://open.spotify.com/",
+	},
+	{
+		image: tempImage,
+		title: "Title Episode",
+		episodeNumber: 3,
+		seasonNumber: 1,
+		tags: ["Supernova", "Jupiter", "Astronaut"],
+		description: `ipsum dolor sit amet, consectetur adipiscing elit. Urna,
+            imperdiet adipiscing libero commodo egestas ac pretium, at.
+            Viverra viverra quam non aliquet. Nec volutpat in morbi
+            molestie sem porttitor massa. Massa ac viverra montes,
+            maecenas tempus, lorem ultrices.`,
+		datePosted: "02/08/2023",
+		duration: "1 hour and 3 min",
+		youtubeLink: "https://www.youtube.com/",
+		spotifyLink: "https://open.spotify.com/",
+	},
 ];
