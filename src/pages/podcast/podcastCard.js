6151f56c386fc01d4814ea49d497ca7ebf360cca
Eric Matlock <ermatlock@gmail.com>
2023-02-17 23:53:59 -0700

    Update styling and add link icons

--- src/pages/podcast/podcastCard.js
+++ src/pages/podcast/podcastCard.js
@@ -1,19 +1,23 @@
 import React from "react";
+import applePodcast from "../../assets/podcastAssets/apple-podcasts.svg";
+import spotify from "../../assets/podcastAssets/spotify.svg";
+import youtube from "../../assets/podcastAssets/youtube.svg";
 import "./podcast.scss";
 
-export default function podcastCard({
-	image,
-	title,
-	episodeNumber,
-	seasonNumber,
-	tags,
-	description,
-	datePosted,
-	duration,
-	youtubeLink,
-	spotifyLink,
-}) {
-	// console.log("🚀 ~ file: podcastCard.js:16 ~ tags", tags);
+export default function podcastCard({ card }) {
+	const {
+		image,
+		title,
+		episodeNumber,
+		seasonNumber,
+		tags,
+		description,
+		datePosted,
+		duration,
+		youtubeLink,
+		spotifyLink,
+	} = card;
+
 	const getTagColor = (tag) => {
 		console.log("🚀 ~ file: podcastCard.js:18 ~ getTagColor ~ tag", tag);
 		if (tag === "Planets") {
@@ -25,9 +29,7 @@ export default function podcastCard({
 		}
 	};
 
-	const getChips = (tags) => {
-		
-	};
+	const getChips = (tags) => {};
 
 	return (
 		<div className="podcast-card">
@@ -36,28 +38,46 @@ export default function podcastCard({
 					src={image}
 					alt="cute astronauts"
 					style={{
-						height: 321,
-						width: 321,
+						height: 350,
+						width: 350,
 						display: "block",
 						objectFit: "cover",
 					}}
 				/>
 			</div>
 			<div className="info-box">
-				<h2 style={{margin: 0, padding: "0rem 0rem .5rem 0rem"}}>{`${title}: (S${seasonNumber}E${episodeNumber})`}</h2>
-				<div style={{display: "flex"}}>
+				<h2
+					style={{ margin: 0, padding: "0rem 0rem .5rem 0rem" }}
+				>{`${title}: (S${seasonNumber}E${episodeNumber})`}</h2>
+				<div style={{ display: "flex" }}>
 					{tags.map((tag) => {
 						return (
-							<div className="chip" id={`chip-${tag}`} style={{backgroundColor: `${getTagColor(tag)}`}} >
+							<div
+								className="chip"
+								id={`chip-${tag}`}
+								style={{
+									backgroundColor: `${getTagColor(tag)}`,
+								}}
+							>
 								<p>{tag}</p>
 							</div>
 						);
 					})}
 				</div>
 				<h4>About:</h4>
-				<p>{description}</p>
+				<p className="description">{description}</p>
 				<p style={{ marginTop: "1rem" }}>{duration}</p>
 				<p>{datePosted}</p>
+				<div style={{ display: "flex" }}>
+					<a className="stream-button" href={youtubeLink} target="_blank" rel="noreferrer">
+						<img src={youtube} />
+						<p>Listen on YouTube</p>
+					</a>
+					<a className="stream-button" href={spotifyLink} target="_blank" rel="noreferrer">
+						<img src={spotify} />
+						<p>Listen on Spotify</p>
+					</a>
+				</div>
 			</div>
 		</div>
 	);
