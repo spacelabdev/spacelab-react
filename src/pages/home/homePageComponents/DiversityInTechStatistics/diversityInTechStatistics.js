e673de3ddaa4343f29ab48a95831c87b31a49243
Meghan Bucher <115611931+bardIRL@users.noreply.github.com>
2023-04-26 21:17:24 -0700

    updates DiversityInTechStatistics component to include updated stats/text content from wireframe

--- src/pages/home/homePageComponents/DiversityInTechStatistics/diversityInTechStatistics.js
+++ src/pages/home/homePageComponents/DiversityInTechStatistics/diversityInTechStatistics.js
@@ -16,37 +16,37 @@ export default function DiversityInTechStatistics() {
 					<h3>Them</h3>
 					<div className="stat-card">
 						<p className="percentage ">26%</p>
-						<p className="text">Percentage of Women in Tech</p>
+						<p className="text">Women in Tech</p>
 					</div>
 					<div className="stat-card">
 						<p className="percentage ">35%</p>
-						<p className="text">
-							Percentage of People of Color in Tech
-						</p>
+						<p className="text">People of Color in Tech</p>
 					</div>
 					<div className="stat-card">
-						<p className="percentage ">6%</p>
+						<p className="percentage ">12%</p>
 						<p className="text">
-							Percentage of People with Disabilities in Tech
+							General Unemployment Rate for Those with
+							Disabilities
 						</p>
 					</div>
 				</div>
 				<div className="stats-column spacelab">
 					<h3>SpaceLab</h3>
 					<div className="stat-card">
-						<p className="percentage ">60%</p>
-						<p className="text">Percentage of Women at SpaceLab</p>
-					</div>
-					<div className="stat-card">
-						<p className="percentage ">50%</p>
+						<p className="percentage ">65%</p>
 						<p className="text">
-							Percentage of People of Color at SpaceLab
+							Women/Non-Binary Members at SpaceLab
 						</p>
 					</div>
 					<div className="stat-card">
-						<p className="percentage ">30%</p>
+						<p className="percentage ">60%</p>
+						<p className="text">People of Color at SpaceLab</p>
+					</div>
+					<div className="stat-card">
+						<p className="percentage ">17%</p>
 						<p className="text">
-							Percentage of People with Disabilities at SpaceLab
+							People Who Identify with Having a Disability at
+							SpaceLab
 						</p>
 					</div>
 				</div>
