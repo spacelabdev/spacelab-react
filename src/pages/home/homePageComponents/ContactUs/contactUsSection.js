addbb6c8f5e1758411a4de8ffbd8e1964fe4aed7
Meghan Bucher <115611931+bardIRL@users.noreply.github.com>
2023-04-26 17:34:39 -0700

    styles contact us section on home page, adds image

--- src/pages/home/homePageComponents/ContactUs/contactUsSection.js
+++ src/pages/home/homePageComponents/ContactUs/contactUsSection.js
@@ -1,29 +1,32 @@
 import React from "react";
 import "./contactUsSection.scss";
 import InternalNavButton from "../../../../components/styleComponents/navigationButtons/internalNavButton";
+import ContactUsImage from "../../../../assets/homeAssets/image5.png";
 
 export default function ContactUsSection() {
 	return (
-		<section>
-			<h2>Want to be a part of the team?</h2>
-			<p>
-				As diversity fuels creativity and innovation, we here at
-				SpaceLab strongly value the voices and perspectives of those
-				underrepresented in the community. One of our goals is to create
-				paid fellowships, internships, scholarship opportunities, and
-				debt relief for those who are underrepresented, including but
-				not limited to women, members of the LGBTQ+ community, and
-				people of color. Helping members of the community who struggle
-				economically will provide them with the liberty and flexibility
-				necessary to explore new career paths in science and technology.
-				To help us grow these opportunities, please consider making a
-				tax-deductible donation to SpaceLab today.
-			</p>
-			<div className="button-wrap">
-				<InternalNavButton
-					path="/contact"
-					buttonText="Contact Us"
-				></InternalNavButton>
+		<section id="contact-us-section">
+			<img src={ContactUsImage} className="contact-us-img" />
+			<div className="text-content">
+				<h2>Want to be a part of the team?</h2>
+				<p>
+					Are you fascinated by the mysteries of outer space? Do you
+					dream of being a part of the exploration and discovery of
+					our universe? If so, we invite you to join our organization
+					as a volunteer! As a volunteer, you will have the
+					opportunity to contribute to groundbreaking projects,
+					participate in educational programs, and engage with the
+					public about the wonders of outer space. You'll work
+					alongside some of the brightest minds in the field, and gain
+					valuable skills and experience that will serve you well in
+					any future endeavor.
+				</p>
+				<div className="button-wrap">
+					<InternalNavButton
+						path="/contact"
+						buttonText="Contact Us"
+					></InternalNavButton>
+				</div>
 			</div>
 		</section>
 	);
