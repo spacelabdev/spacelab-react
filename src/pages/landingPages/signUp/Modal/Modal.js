05e155fe82bb375b2296217c98a20bfe93eab201
Cindy Wong <cindywongdev@gmail.com>
2023-05-17 12:26:46 -0400

    added homepage under modal

--- src/pages/landingPages/signUp/Modal/Modal.js
+++ src/pages/landingPages/signUp/Modal/Modal.js
@@ -1,14 +1,16 @@
-import './Modal.scss'
+import "./Modal.scss"
 
-export default function Modal(props){
+export default function Modal({open, onClose}){
+    if (!open) return null
+
     return (
-        <div className="signup-modal">
-            <h3 className="closeBtn">X</h3>
-            <div className="content">
-                <h1> Let's Stay in Orbit!</h1>
-                <h3 className="CTA-small-text">Subscribe now, and join our community of space enthusiasts</h3>
-                {/* email input goes here */}
+            <div className="modal-container">
+                <h3 onClick={onClose} className="close-modal">X</h3>
+                <div className="modal-text">
+                    <h1 className="modal-heading"> Let's Stay in Orbit!</h1>
+                    <h3 className="modal-subheading">Subscribe now, and join our community of space enthusiasts</h3>
+                    {/* email input goes here */}
+                </div>
             </div>
-        </div>
     )
 }
--- src/pages/landingPages/signUp/SignUp.js
+++ src/pages/landingPages/signUp/SignUp.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import Footer from "../../../components/footer/footer";
 import "./signUp.scss";
-import Navigation from '../../../components/navigation/mainNavMenu/navigation'
-import Modal from './Modal/Modal'
+import Navigation from "../../../components/navigation/mainNavMenu/navigation"
+import Modal from "./Modal/Modal"
+import Home from "../../home/home"
 
 /**
  * Renders Sign Up Landing page
@@ -10,11 +11,15 @@ import Modal from './Modal/Modal'
  * @constructor
  */
 export default function SignUp() {
+	const [openModal, setOpenModal] = useState(true)
+
 	return (
 		<>
-			<Navigation/>
-			<Modal />
-			<Footer />
+			<Navigation className="modal-nav"/>
+			<div className="overlay">
+				<Modal open={openModal} onClose={() => setOpenModal(false)} />
+				<Home/>
+			</div>
 		</>
 	);
 }
