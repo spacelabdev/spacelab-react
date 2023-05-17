9589094be7186eb5e705af64336a559ce543b5f3
Cindy Wong <cindywongdev@gmail.com>
2023-05-17 11:51:48 -0400

    added content to modal

--- src/pages/landingPages/signUp/Modal/Modal.js
+++ src/pages/landingPages/signUp/Modal/Modal.js
@@ -2,8 +2,13 @@ import './Modal.scss'
 
 export default function Modal(props){
     return (
-        <div class="signup-modal">
-            <h1>Modal Component</h1>
+        <div className="signup-modal">
+            <h3 className="closeBtn">X</h3>
+            <div className="content">
+                <h1> Let's Stay in Orbit!</h1>
+                <h3 className="CTA-small-text">Subscribe now, and join our community of space enthusiasts</h3>
+                {/* email input goes here */}
+            </div>
         </div>
     )
 }
