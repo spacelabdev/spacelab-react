e4a494ce65f85f457e9cd20886e8c805f0e64aaf
Meghan Bucher <115611931+bardIRL@users.noreply.github.com>
2023-05-18 10:24:29 -0700

    creates What We're Working On componenent, adds Participate landing page stylesheet

--- src/pages/landingPages/participate/Participate.js
+++ src/pages/landingPages/participate/Participate.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Navigation from "../../../components/navigation/mainNavMenu/navigation";
 import Hero from "./Hero/Hero";
+import WhatWereWorkingOn from "./WhatWereWorkingOn/WhatWereWorkingOn";
 import Footer from "../../../components/footer/footer";
+import "./Participate.scss";
 
 /**
  * Renders Participate Landing page
@@ -10,10 +12,11 @@ import Footer from "../../../components/footer/footer";
  */
 export default function Participate() {
 	return (
-		<>
+		<div className="participate-landing-page">
 			<Navigation />
 			<Hero />
+			<WhatWereWorkingOn />
 			<Footer />
-		</>
+		</div>
 	);
 }
