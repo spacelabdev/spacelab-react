70b1464c74902c7caf5d744cbcc4a30fd643fd46
Deepthi Srinidhi Rao <deepthi2609@gmail.com>
2023-05-22 11:34:16 -0700

    Added title section implementation

--- src/pages/landingPages/donate/aboutOrgIdentitySection/aboutOrgIdentitySection.js
+++ src/pages/landingPages/donate/aboutOrgIdentitySection/aboutOrgIdentitySection.js
@@ -4,14 +4,16 @@ import "./aboutOrgIdentitySection.scss";
 const AboutOrgIdentitySection = () => {
 	return (
 		<section id="about-org-identity-container">
-			{SPACELAB_ORG_INDENTITY.map(({ title, description }) => (
-				<div className="card-section" key={title}>
-					<h4 className="donate-title">{title}</h4>
-					<div className="about-mission-content">
-						<p>{description}</p>
+			<div className="about-org-content">
+				{SPACELAB_ORG_INDENTITY.map(({ title, description }) => (
+					<div className="card-section" key={title}>
+						<h4 className="donate-title">{title}</h4>
+						<div className="about-mission-content">
+							<p>{description}</p>
+						</div>
 					</div>
-				</div>
-			))}
+				))}
+			</div>
 		</section>
 	);
 };
