c5a6124dbff5a3bf16161fb83f6f5e7c0cce3969
annakreno <annakreno@gmail.com>
2023-05-24 20:31:41 -0500

    removing duplicate lines of code

--- src/pages/landingPages/signUp/CallToAction/SignUpForm/SignUpForm.js
+++ src/pages/landingPages/signUp/CallToAction/SignUpForm/SignUpForm.js
@@ -28,14 +28,11 @@ export default function SignUpForm() {
     return (
         <div className="sign-up-button-container">
             <div className="radial-gradient-container"
-            <div className="radial-gradient-container"
                 onMouseMove={handleMouseMove}
                 style={{
                     background: `radial-gradient(
                         circle at ${mousePosition.x}px ${mousePosition.y}px, 
                         rgba(255, 255, 255, 0) 0%,
-                        rgba(8,22,51,255) 15%,
-                        rgba(8,22,51,255) 100%`
                         rgba(8,22,51,255) 15%,
                         rgba(8,22,51,255) 100%`
                     }}
@@ -67,32 +64,6 @@ export default function SignUpForm() {
 
             </div>
         </div>
-                <form
-                    className="sign-up-subscription-form"
-                    onSubmit={handleSubmit}
-                >
-                    <input
-                        className="sign-up-email-input"
-                        type="email"
-                        placeholder="ENTER EMAIL TO SUBSCRIBE"
-                        name="email"
-                        required
-                    />
-                    <ValidationError
-                        prefix="Email"
-                        field="email"
-                        errors={state.errors}
-                    />
-                    <button
-                        className="sign-up-email-submit"
-                        type="submit"
-                        disabled={state.submitting}
-                    >
-                        <img src={arrowForward} className="submit-arrow-image" alt="arrow icon"></img>
-                    </button>
-                </form>
-
-            </div>
-        </div>
+                
     )
 }
