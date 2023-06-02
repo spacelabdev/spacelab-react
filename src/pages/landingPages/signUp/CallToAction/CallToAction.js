be6c267bbd04dd8672e84cd765d4c00bbecaef4b
annakreno <annakreno@gmail.com>
2023-06-02 15:06:09 -0500

    adjusting styling and add X link to home on Thank You page

--- src/pages/landingPages/signUp/CallToAction/CallToAction.js
+++ src/pages/landingPages/signUp/CallToAction/CallToAction.js
@@ -1,7 +1,9 @@
 import "./CallToAction.scss";
 import React, { useState } from "react";
 import planetImage from "../assets/solen-feyissa-54xmXW7tJzo-unsplash.png";
+import closeX from "../assets/close.png"
 import SignUpForm from "./SignUpForm/SignUpForm";
+import { Link } from 'react-router-dom';
 
 export default function CallToAction() {
   const [success, setSuccess] = useState(false);
@@ -12,38 +14,48 @@ export default function CallToAction() {
 
   return (
     <div className="sign-up-landing-page-section cta-container">
-      <div className="cta-text">
         {success ? (
-          <>
-            <div className="submission-successful-text">
-              <span className="submission-successful-heading">Thank You!</span>
-              <br />
-              <span className="submission-successful-subheading">
-                You have now subscribed to our email newsletter.
-                You should receive a confirmation email soon. Ad Astra!🚀
-              </span>
+            <div className={`cta-text ${success ? 'success-state' : ''}`}>
+                <div className="bounding-box">
+                    <Link 
+                        to={"/"}
+                        style={{ textDecoration: "none" }}
+                    >
+                        <img
+                            src={closeX}
+                            className="close-png"
+                            alt="close png"
+                        />
+                    </Link>
+                </div>
+                <span className="cta-heading">Thank You!</span>
+                <br />
+                <span className="cta-subheading">
+                    You have now subscribed to our email newsletter. You should receive
+                    <br/>
+                    a confirmation email soon. Ad Astra!🚀
+                </span>
             </div>
-          </>
         ) : (
-          <>
-            <span className="cta-heading">Let's Stay In Orbit!</span>
-            <span className="cta-subheading">
-              Subscribe now, and join our community of space enthusiasts.
-            </span>
-          </>
+            <div className="cta-text">
+                <span className="cta-heading">Let's Stay In Orbit!</span>
+                <span className="cta-subheading">
+                    Subscribe now, and join our community of space enthusiasts.
+                </span>
+            </div>
         )}
-      </div>
 
-      {!success && <SignUpForm onSuccess={handleSuccess} />}
+        {!success && <SignUpForm onSuccess={handleSuccess}/>}
 
-      <div className="planet-image-wrapper">
-        <img
-          src={planetImage}
-          className="planet-image"
-          alt="blue planet art by Solen Feyissa"
-        />
-        <div className="planet-bg-glow"></div>
-      </div>
+        <div className="planet-image-wrapper">
+            <img
+                src={planetImage}
+                className="planet-image"
+                alt="blue planet art by Solen Feyissa"
+            />
+            <div className="planet-bg-glow"></div>
+        </div>
+
     </div>
   );
 }
