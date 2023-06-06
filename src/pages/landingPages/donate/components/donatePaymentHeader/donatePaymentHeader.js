7e2d39f75e0123605205cc4e87259869407be5d6
laurenhatchett <laurenhatchett@gmail.com>
2023-06-06 08:02:43 -0400

    add separate images in header

--- src/pages/landingPages/donate/components/donatePaymentHeader/donatePaymentHeader.js
+++ src/pages/landingPages/donate/components/donatePaymentHeader/donatePaymentHeader.js
@@ -1,11 +1,15 @@
 import React from "react";
-import donate_payment_group from "../../assets/donate_payment_group.png"
+import group_of_two_men from '../../assets/group_of_two_men.png'
+import three_women_chatting from '../../assets/three_women_chatting.png'
+import one_woman_talking_and_one_laughing from '../../assets/one_woman_talking_and_one_laughing.png'
 
 const DonatePaymentHeader = () => {
   return (
     <>
     <div className="donate-payment-header-container">
-      <img src={donate_payment_group} alt='group-of-people' />
+      <img src={group_of_two_men} alt='group-of-two-men' />
+      <img src={three_women_chatting} alt='three-women-chatting'/>
+      <img src={one_woman_talking_and_one_laughing} alt='one-woman-talking-and-one-laughing'/>
     </div>
     </>
   )
