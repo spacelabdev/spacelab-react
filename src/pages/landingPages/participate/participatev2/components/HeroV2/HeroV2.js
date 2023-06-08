0d7f271a5efaaa6d764902c922e769ccc7c49cfc
Jaspreet <jaspreetkn92@gmail.com>
2023-06-08 15:00:29 +0100

    added HeroV2 component

---
+++ src/pages/landingPages/participate/participatev2/components/HeroV2/HeroV2.js
@@ -0,0 +1,27 @@
+import React from "react";
+import HeroImage from "../../assets/HeroImage.png";
+import { Link } from "react-router-dom";
+import "./HeroV2.scss";
+
+export default function HeroV2() {
+	return (
+		<section
+			className="participate-heroV2"
+			style={{ backgroundImage: `url(${HeroImage})` }}
+		>
+			<div>
+				<h2 className="heading">Join us at SpaceLab!</h2>
+			</div>
+
+			<div>
+				<p className="header-text">
+					Outerspace tech projects to excel your career
+				</p>
+			</div>
+
+			<button>
+				<Link to={"/campaigns/participate/apply"}>Apply Now</Link>
+			</button>
+		</section>
+	);
+}
