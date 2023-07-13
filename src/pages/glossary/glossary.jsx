bc38df874fce78eca733ab811df4b5aed5773642
Laura Hiller <Hillerelaura@gmail.com>
2023-07-12 18:33:26 -0700

    Last minute fixes

--- src/pages/glossary/glossary.jsx
+++ src/pages/glossary/glossary.jsx
@@ -4,7 +4,7 @@ import HeroImage from "../../components/HeroImage/HeroImage";
 import GlossaryList from "./GlossaryList";
 import Footer from "../../components/Footer/Footer";
 
-/**
+/** Glossary Page
  * @returns {JSX.Element}
  * @constructor
  */
