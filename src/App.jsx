4d1cc547a589b4710b250658097234aea7aafbaa
Laura Hiller <Hillerelaura@gmail.com>
2023-07-17 15:08:05 -0700

    Renamed glossaryHelper

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import RSSParser from "rss-parser";
 import {
 	glossaryTermsArray,
 	returnFilteredTerms,
-} from "./pages/Glossary/glossaryHelper.js";
+} from "./pages/Glossary/glossaryhelper.js";
 import "./App.scss";
 import Main from "./main";
 
