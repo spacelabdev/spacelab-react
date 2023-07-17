cf97808851174c0caf163fe92de52409aa115df3
Shirin Rokni <shirinrokni@Shirins-MacBook-Pro.local>
2023-07-17 16:53:46 -0700

    added overlay, made popup more refined, close button functionality

--- src/pages/about/teamSlider/memberCard/memberCard.js
+++ src/pages/about/teamSlider/memberCard/memberCard.js
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { IconContext } from "react-icons";
-import {
-	AiOutlineGithub,
-	AiOutlineClose,
-} from "react-icons/ai";
+import { AiOutlineGithub } from "react-icons/ai";
 import {FaBriefcase, FaLinkedin} from "react-icons/fa";
+import {MdClose, MdOutlineAlarmAdd} from "react-icons/md";
 import Astronauts from "../../../../assets/ju-guan-D-jLxBtEwaA-unsplash.jpg";
 import "./memberCard.scss";
 
@@ -26,6 +24,16 @@ const MemberCard = (memberObject) => {
 	// State to track if the details container should be displayed
 	const [showDetails, setShowDetails] = useState(false);
 
+	useEffect(() => {
+		if (showDetails) {
+		  // Prevent scrolling open
+		  document.body.style.overflow = "hidden";
+		} else {
+		  // Re-enable scrolling when closed
+		  document.body.style.overflow = "auto";
+		}
+	}, [showDetails]);
+
 	if (!member.image) {
 		image = Astronauts;
 	}
@@ -43,63 +51,78 @@ const MemberCard = (memberObject) => {
 
 	// Function to handle the card click event
 	const handleCardClick = () => {
-		setShowDetails(!showDetails); 
+		if (!showDetails) {
+			setShowDetails(true);
+		}
+	};
+
+	// function to close card
+	const handleCloseClick = () => {
+		setShowDetails(false);
+		console.log("closed");
 	};
 
 	return (
-		<div className="member-card" onClick={handleCardClick}>
-			<img src={image} alt={`${member.fullName}.png`} />
+		<div className="member-card">
+			<img src={image} alt={`${member.fullName}.png`} onClick={handleCardClick}/>
 			<div className="member-card-text-container">
 				<p className="member-card-text-name">{member.fullName}</p>
 				<p className="member-card-text-title">{member.title}</p>
 			</div>
-			{/* adding the pop-up modal */}
-			<div className="member-card-modal-overlay"></div> 
+			{/* overlay layer */}
+			{showDetails && (<div className="overlay" onClick={handleCloseClick}></div>)}
 			<div className={`member-card-details-container ${showDetails ? "show" : ""}`}>
-				<p className="member-card-text-name">{member.fullName}</p>
-				<p className="member-card-text-title">{member.title}</p>
-				<p className="member-card-text-quote">{member.bioQuote}</p>
-				<div className={"member-card-link-container"}>
-					<IconContext.Provider
-						value={{ color: "var(--text)", size: "2rem" }}
-					>
-						<a
-							className={`member-card-github ${
-								hasLinkedIn ? "" : "hidden"
-							}`}
-							href={member.linkedIn}
-							target="_blank"
-							rel="noreferrer"
-						>
-							<FaLinkedin />
-						</a>
-						<a
-							className={`member-card-github ${
-								hasGitHub ? "" : "hidden"
-							}`}
-							href={member.gitHub}
-							target="_blank"
-							rel="noreferrer"
-						>
-							<AiOutlineGithub />
-						</a>
-						<a
-							className={`member-card-github ${
-								hasPortfolio ? "" : "hidden"
-							}`}
-							href={member.portfolio}
-							target="_blank"
-							rel="noreferrer"
-						>
-							<FaBriefcase />
-						</a>
-
+				<img src={image} alt={`${member.fullName}.png`} className="member-card-details-image" />
+				<div class="member-card-right-side">
+					<IconContext.Provider value={{ color: "var(--text)", size: "2rem" }} >
+					<div className="member-card-close" onClick={handleCloseClick}> <MdClose /> </div>
 					</IconContext.Provider>
+					<p className="member-card-text-name">{member.fullName}</p>
+					<p className="member-card-text-title">{member.title}</p>
+					<p className="member-card-text-quote">{member.bioQuote}</p>
+					<div className={"member-card-link-container"}>
+						<IconContext.Provider
+							value={{ color: "var(--text)", size: "2rem" }}
+						>
+							<a
+								className={`member-card-github ${
+									hasLinkedIn ? "" : "hidden"
+								}`}
+								href={member.linkedIn}
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FaLinkedin />
+							</a>
+							<a
+								className={`member-card-github ${
+									hasGitHub ? "" : "hidden"
+								}`}
+								href={member.gitHub}
+								target="_blank"
+								rel="noreferrer"
+							>
+								<AiOutlineGithub />
+							</a>
+							<a
+								className={`member-card-github ${
+									hasPortfolio ? "" : "hidden"
+								}`}
+								href={member.portfolio}
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FaBriefcase />
+							</a>
+
+						</IconContext.Provider>
+					</div>
 				</div>
 			</div>
 		</div>
 	);
 };
+
 
 MemberCard.propTypes = {
 	/**
@@ -113,3 +136,11 @@ MemberCard.defaultProps = {
 };
 
 export default MemberCard;
+
+
+/*
+to do:
+	fix text inside box
+	fix text on screen
+	enable scrolling on long text people (amanda)
+*/
