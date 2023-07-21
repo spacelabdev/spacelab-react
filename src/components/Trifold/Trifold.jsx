7aa39fb3602ee763c52c29de030f1d707569fa28
neabbas <neabbas@ucsc.edu>
2023-07-21 12:51:26 -0700

    Added height prop and adjusted Home path in main.jsx

--- src/components/Trifold/Trifold.jsx
+++ src/components/Trifold/Trifold.jsx
@@ -1,5 +1,7 @@
-import React, { useContext } from "react";
+import React from "react";
 import "./Trifold.scss";
+
+
 
 
 /**
@@ -10,26 +12,40 @@ import "./Trifold.scss";
  * @param {String} alt1 alt text for image 1
  * @param {String} alt2 alt text for image 2
  * @param {String} alt3 alt text for image 3
+ * @param {Number} height height for each image in the trifold-- effectively changes the height of the trifold.
+ *                        if not specified, height is automatic based on the width attribute
  * @returns {JSX.Element}
  * @constructor
  */
 
 
-export default function Trifold({image1, alt1, image2, alt2, image3, alt3}) {
+
+
+export default function Trifold({height, image1, alt1, image2, alt2, image3, alt3}) {
     return (
         <div className="images-container">
             <img
+                height = {height}
+                width = "33.3%"
+                object-fit = 'cover'
                 src={image1}
                 alt={alt1}
             />
             <img
+                height = {height}
+                width = "33.3%"
+                object-fit = 'cover'
                 src={image2}
                 alt={alt2}
             />
             <img
+                height = {height}
+                width = "33.3%"
+                object-fit = 'cover'
                 src={image3}
                 alt={alt3}
             />
         </div>
     );
 }
+
