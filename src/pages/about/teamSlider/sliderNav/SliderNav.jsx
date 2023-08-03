e222477e4cdbec932e44ba08c293c6ecfbc9aa3c
Asti <astistateofmind@gmail.com>
2023-08-02 17:42:09 -0700

    adds filtering

--- src/pages/about/teamSlider/sliderNav/SliderNav.jsx
+++ src/pages/about/teamSlider/sliderNav/SliderNav.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import SliderNavLink from "./sliderNavLink";
 import PropTypes from "prop-types";
 import "./sliderNav.scss";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import { AiOutlineDown, AiOutlineRight } from "react-icons/ai";
 
 /**
  * Renders the Nav bar for the sliders
@@ -11,21 +14,50 @@ import "./sliderNav.scss";
  * @returns {JSX.Element}
  * @constructor
  */
-const SliderNav = ({navObjects, activeSlide, setActive}) => {
+const SliderNav = ({ navObjects, activeSlide, setActive }) => {
+	const [anchorEl, setAnchorEl] = useState(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
 	return (
-		<div className={'menu'}>
+		<div className={"menu"}>
 			<div className={`slider-nav-button-container`}>
-				{navObjects.map((navObject) => (
-					<SliderNavLink key={`${navObject.activeFlag}`}
-					               navObject={navObject}
-					               activeSlide={activeSlide}
-					               setActive={setActive}
-					/>
-				))}
+				<h4>Filter</h4>
+
+				<Button
+					id="dropdown-basic"
+					aria-controls={open ? "basic-menu" : undefined}
+					aria-haspopup="true"
+					aria-expanded={open ? "true" : undefined}
+					onClick={handleClick}
+				>
+					{open ? <AiOutlineDown /> : <AiOutlineRight />}
+				</Button>
+				<Menu
+					id="basic-menu"
+					anchorEl={anchorEl}
+					open={open}
+					MenuListProps={{
+						"aria-labelledby": "basic-button",
+					}}
+				>
+					{navObjects.map((navObject) => (
+						<SliderNavLink
+							key={`${navObject.activeFlag}`}
+							navObject={navObject}
+							activeSlide={activeSlide}
+							setActive={setActive}
+							setAnchorEl={setAnchorEl}
+							className="dropdown-item"
+						/>
+					))}
+				</Menu>
 			</div>
 		</div>
 	);
-}
+};
 
 SliderNav.propTypes = {
 	/**
@@ -41,12 +73,12 @@ SliderNav.propTypes = {
 	 * Function to update the activeSlide state variable to a new activeSlide
 	 */
 	setActive: PropTypes.func,
-}
+};
 
 SliderNav.defaultProps = {
 	navObjects: null,
 	activeSlide: null,
 	setActive: null,
-}
+};
 
 export default SliderNav;
