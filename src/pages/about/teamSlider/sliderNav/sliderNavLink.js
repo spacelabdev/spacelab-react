76bc9a9f0a76881eb8cc43e933372ec09ed90ddf
Gav1012 <gavin.poley@gmail.com>
2023-08-08 15:47:24 -0700

    staging new files

---
+++ src/pages/about/teamSlider/sliderNav/sliderNavLink.js
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./sliderNav.scss";
+import MenuItem from '@mui/material/MenuItem';
+
+/**
+ * Creates each slider nav link JSX. Rendered by SliderNav
+ * @param navObject
+ * @param activeSlide
+ * @param setActive
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const SliderNavLink = ({ navObject, activeSlide, setActive, setAnchorEl}) => {
+	const handleClose = () => {
+		setActive(navObject.activeFlag);
+		setAnchorEl(null)
+	}
+	return (
+		<MenuItem className={`${activeSlide === navObject.activeFlag ? 'active' : ''}`}
+			onClick={() => {
+				handleClose()
+			}
+			}
+		>
+			{navObject.title}
+			</MenuItem>
+	);
+}
+
+SliderNavLink.propTypes = {
+	/**
+	 * Objects containing the title to be displayed as the nav link,
+	 * and the flag that is check to see if the nav link should be underlined as active.
+	 */
+	navObject: PropTypes.object,
+	/**
+	 * State which is set to the current active slide
+	 */
+	activeSlide: PropTypes.string,
+	/**
+	 * Function to update the activeSlide state variable to a new activeSlide
+	 */
+	setActive: PropTypes.func,
+}
+
+SliderNavLink.defaultProps = {
+	navObjects: null,
+	activeSlide: null,
+	setActive: null,
+}
+
+export default SliderNavLink;
