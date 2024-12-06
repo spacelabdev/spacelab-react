78344208c376d7e8e57d91c3758370f743100fc9
FJ Tria <fjstria@gmail.com>
2024-12-05 17:32:00 -0800

    WIP diversity stats

--- src/pages/Home/homePageComponents/DiversityInTechStatistics/DiversityInTechStatistics.jsx
+++ src/pages/Home/homePageComponents/DiversityInTechStatistics/DiversityInTechStatistics.jsx
@@ -4,50 +4,44 @@ import earthImage from "../../../../assets/homeAssets/image4.jpg";
 
 export default function DiversityInTechStatistics() {
 	return (
-		<section
-			style={{
-				backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0), #1c1a22), url(${earthImage})`,
-			}}
-			className="diversity-in-tech-stats-section"
-		>
-			<h2>Diversity in Tech: Statistics</h2>
+		<section className="diversity-in-tech-stats-section">
+			<h2>DIVERSITY DONE RIGHT</h2>
 			<div className="stats-wrapper">
+				
+				<div className="stats-column spacelab">
+					<h3>The Spacelab Nonprofit</h3>
+					<div className="stat-card">
+						<p className="percentage ">65%</p>
+						<p className="text">
+							Women/Non-Binary Members
+						</p>
+					</div>
+					<div className="stat-card">
+						<p className="percentage ">60%</p>
+						<p className="text">Identify as People of Color</p>
+					</div>
+					<div className="stat-card">
+						<p className="percentage ">17%</p>
+						<p className="text">
+							Identify with Having a Disability
+						</p>
+					</div>
+				</div>
+
 				<div className="stats-column them">
-					<h3>Them</h3>
+					<h3>Other Tech Companies</h3>
 					<div className="stat-card">
 						<p className="percentage ">26%</p>
 						<p className="text">Women in Tech</p>
 					</div>
 					<div className="stat-card">
 						<p className="percentage ">35%</p>
-						<p className="text">People of Color in Tech</p>
+						<p className="text">Identify as People of Color</p>
 					</div>
 					<div className="stat-card">
 						<p className="percentage ">12%</p>
 						<p className="text">
-							General Unemployment Rate for Those with
-							Disabilities
-						</p>
-					</div>
-				</div>
-				<h3>VS.</h3>
-				<div className="stats-column spacelab">
-					<h3>SpaceLab</h3>
-					<div className="stat-card">
-						<p className="percentage ">65%</p>
-						<p className="text">
-							Women/Non-Binary Members at SpaceLab
-						</p>
-					</div>
-					<div className="stat-card">
-						<p className="percentage ">60%</p>
-						<p className="text">People of Color at SpaceLab</p>
-					</div>
-					<div className="stat-card">
-						<p className="percentage ">17%</p>
-						<p className="text">
-							People Who Identify with Having a Disability at
-							SpaceLab
+							Disability Unemployment Rate
 						</p>
 					</div>
 				</div>
