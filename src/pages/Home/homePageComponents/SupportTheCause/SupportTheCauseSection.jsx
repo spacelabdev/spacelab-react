f1bec550c98fa643937081a7b16cf01bcc664c9c
FJ Tria <fjstria@gmail.com>
2024-12-05 18:18:26 -0800

    Support the cause section

--- src/pages/Home/homePageComponents/SupportTheCause/SupportTheCauseSection.jsx
+++ src/pages/Home/homePageComponents/SupportTheCause/SupportTheCauseSection.jsx
@@ -12,30 +12,27 @@ export default function SupportTheCauseSection() {
 		<div id={"support-the-cause-section"}>
 			<div className={"support-the-cause-wrap"}>
 				<h2 className={"support-the-cause-header"}>
-					Support The Cause
+					SUPPORT THE CAUSE
 				</h2>
 				<p>
-					As diversity fuels creativity and innovation, we here at
-					SpaceLab strongly value the voices and perspectives of those
-					underrepresented in the community.
+					At Spacelab, we believe that diversity drives innovation and creativity, which is
+					why we value the voices and perspectives of those underrepresented in the community.
 				</p>
 				<p>
-					One of our goals is to create paid fellowships, internships,
-					scholarship opportunities, and debt relief for those who are
-					underrepresented, including but not limited to women,
+					Our goal is to create paid fellowships, internships,
+					scholarship opportunities, and provide debt relief to these
+					underrepresented groups – including women,
 					members of the <span title="lesbian, gay, bisexual, transgender, queer or questioning, intersex, and asexual">LGBTQ+</span> community, and people of color.
-					Helping members of the community who struggle economically
-					will provide them with the liberty and flexibility necessary
-					to explore new career paths in science and technology.{" "}
+					Giving these groups financial support will afford them the opportunity
+					to explore new career paths in science and technology.
 				</p>
 				<p>
-					To help us grow these opportunities, please consider making
-					a tax-deductible donation to SpaceLab today.
+					Help us achieve this goal by making a donation today!
 				</p>
 				<div id={"donate-button-container"}>
 					<InternalNavButton
 						path="/donate"
-						buttonText="Donate"
+						buttonText="Donate Now"
 					></InternalNavButton>
 				</div>
 			</div>
