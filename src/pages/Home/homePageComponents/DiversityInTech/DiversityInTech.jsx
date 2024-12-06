d442b34dfae67fe891466dd1e2b4ad8a9128fc24
FJ Tria <fjstria@gmail.com>
2024-12-05 16:46:58 -0800

    Diversity in tech/about section

--- src/pages/Home/homePageComponents/DiversityInTech/DiversityInTech.jsx
+++ src/pages/Home/homePageComponents/DiversityInTech/DiversityInTech.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import image1 from "../../../../assets/homeAssets/image1.jpg";
-import image2 from "../../../../assets/homeAssets/image2.jpg";
+// import image1 from "../../../../assets/homeAssets/image1.jpg";
+// import image2 from "../../../../assets/homeAssets/image2.jpg";
 import image3 from "../../../../assets/homeAssets/image3.jpg";
 import InternalNavButton from "../../../../components/styleComponents/navigationButtons/InternalNavButton";
 import "./diversityInTech.scss";
@@ -14,34 +14,29 @@ export default function DiversityInTechSection() {
 	return (
 		<section className="diversity-in-tech-section">
 			<div className="content-container">
-				<div className="images-container">
-					<img
-						src={image1}
-						alt="A group of engineers gatered around a laptop"
-					/>
-					<img
-						src={image2}
-						alt="A person putting post-it notes on a wall"
-					/>
-					<img
-						src={image3}
-						alt="A group of team members sitting at a table with their computers"
-					/>
-				</div>
+				
 				<div className="about">
+					<h3>About</h3>
 					<p>
-						SpaceLab is a people-incubator focusing on outer-space
-						tech initiatives. We support and help underrepresented
-						communities gain experience with real-world projects to
-						help diversify the tech space!
+						Spacelab is a program dedicated to outer-space tech initiatives while 
+						promoting diversity in the tech space! We empower underrepresented communities
+						by providing them with hands-on experience in real-world projects. Through our program,
+						these tech enthusiasts become equipped with the skills and opportunities needed
+						to thrive in the tech industry.
 					</p>
 					<div className="button-wrap">
 						<InternalNavButton
 							path="/about"
-							buttonText="About Us"
+							buttonText="Learn More"
 						></InternalNavButton>
 					</div>
 				</div>
+				
+					<img
+						src={image3}
+						alt="A group of team members sitting at a table with their computers"
+					/>
+
 			</div>
 		</section>
 	);
