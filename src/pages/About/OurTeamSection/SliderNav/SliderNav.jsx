d7ab10443bddb99312de5007cf22bdade566d0f6
Alex-dev7 <rusualexei1995@gmail.com>
2024-12-14 16:16:24 -0500

    updated team section

--- src/pages/About/OurTeamSection/SliderNav/SliderNav.jsx
+++ src/pages/About/OurTeamSection/SliderNav/SliderNav.jsx
@@ -30,6 +30,19 @@ const SliderNav = ({
 	const handleChange = (event) => {
 		const updatedStatus = event.target.value;
 		setStatus(updatedStatus);
+		if (updatedStatus === "all") {
+			setShowAll(true);
+			setShowCurrent(false);
+			setShowPast(false);
+		  } else if (updatedStatus === "current") {
+			setShowAll(false);
+			setShowCurrent(true);
+			setShowPast(false);
+		  } else if (updatedStatus === "past") {
+			setShowAll(false);
+			setShowCurrent(false);
+			setShowPast(true);
+		  }
 	};
 	const handleTeamChange = (event) => {
 		const updatedTeam = event.target.value;
@@ -47,124 +60,184 @@ const SliderNav = ({
 	return (
 		<div className={"menu"}>
 			<div className={`slider-nav-button-container`}>
-				<Toolbar
-					sx={{
-						width: "100%",
-					}}
-				>
-					<h4>Filter</h4>
-					<FormControl
-						id="select-form"
-						sx={{
-							width: "100%",
-						}}
-					>
-						<Select
-							sx={{
-								color: "white",
-								".MuiOutlinedInput-notchedOutline": {
-									border: "none",
-									cursor: "pointer",
-								},
-								"&.Mui-focused .MuiOutlinedInput-notchedOutline":
-									{
-										border: "none",
-										cursor: "pointer",
-									},
-								"&:hover .MuiOutlinedInput-notchedOutline": {
-									border: "none",
-									cursor: "pointer",
-								},
-							}}
-							id="select-dropdown-small"
-							value={activeSlide}
-							onChange={handleTeamChange}
-							IconComponent={MyIcon}
-						>
-							<MenuItem
-								value={"All Teams"}
-								onClick={() => {
-									setActive("All Teams");
-								}}
-							>
-								{"All Teams"}
-							</MenuItem>
+				<div className="status-container" style={{ width: "100%" }}>
+					<h4>Filter by Satus</h4>
+					<form >
+						<div className="status-item">
+							<label>
+								<input
+									type="radio"
+									value="all"
+									checked={status === 'all'}
+									onChange={handleChange}
+								/>
+								All
+							</label>
+						</div>
+						<div className="status-item">
+							<label>
+								<input
+									type="radio"
+									value="current"
+									checked={status === 'current'}
+									onChange={handleChange}
+								/>
+								Current
+							</label>
+						</div>
+						<div className="status-item">
+							<label>
+								<input
+									type="radio"
+									value="past"
+									checked={status === 'past'}
+									onChange={handleChange}
+								/>
+								Past
+							</label>
+						</div>
+					</form>
+				</div>
+				<div className="job-title-container" id="select-form" style={{ width: "100%" }}>
+					<h4>Filter by Job Title</h4>
+					<div className="category-items">
+							<div className="category-item" onClick={() => setActive("All Teams")}>
+								All Teams
+							</div>
 							{navObjects.map((navObject, index) => (
-								<MenuItem
-									key={index}
-									value={navObject.title}
-									onClick={() => {
-										setActive(navObject.title);
-									}}
-								>
-									{navObject.title}
-								</MenuItem>
+							<div
+								className="category-item"
+								key={index}
+								onClick={() => setActive(navObject.title)} 
+							>
+								{navObject.title}
+							</div>
 							))}
-						</Select>
-					</FormControl>
-					<FormControl
-						id="select-form"
-						sx={{
-							width: "100%",
-						}}
-					>
-						<Select
-							sx={{
-								color: "white",
-								".MuiOutlinedInput-notchedOutline": {
-									border: "none",
-									cursor: "pointer",
-								},
-								"&.Mui-focused .MuiOutlinedInput-notchedOutline":
-									{
-										border: "none",
-										cursor: "pointer",
-									},
-								"&:hover .MuiOutlinedInput-notchedOutline": {
-									border: "none",
-									cursor: "pointer",
-								},
-							}}
-							id="select-dropdown"
-							value={status}
-							onChange={handleChange}
-							IconComponent={MyIcon}
-						>
-							<MenuItem
-								value={"all"}
-								onClick={() => {
-									setShowAll(true);
-									setShowCurrent(false);
-									setShowPast(false);
-								}}
-							>
-								All Team Members
-							</MenuItem>
-							<MenuItem
-								value={"current"}
-								onClick={() => {
-									setShowAll(false);
-									setShowCurrent(true);
-									setShowPast(false);
-								}}
-							>
-								Current Team Members
-							</MenuItem>
-							<MenuItem
-								value={"past"}
-								onClick={() => {
-									setShowAll(false);
-									setShowCurrent(false);
-									setShowPast(true);
-								}}
-							>
-								Past Team Members
-							</MenuItem>
-						</Select>
-					</FormControl>
-				</Toolbar>
+					</div>
+
+				</div>
 			</div>
-		</div>
+	  	</div>
+		// <div className={"menu"}>
+		// 	<div className={`slider-nav-button-container`}>
+		// 		<Toolbar
+		// 			sx={{
+		// 				width: "100%",
+		// 			}}
+		// 		>
+		// 			<h4>Filter</h4>
+		// 			<FormControl
+		// 				id="select-form"
+		// 				sx={{
+		// 					width: "100%",
+		// 				}}
+		// 			>
+		// 				<Select
+		// 					sx={{
+		// 						color: "white",
+		// 						".MuiOutlinedInput-notchedOutline": {
+		// 							border: "none",
+		// 							cursor: "pointer",
+		// 						},
+		// 						"&.Mui-focused .MuiOutlinedInput-notchedOutline":
+		// 							{
+		// 								border: "none",
+		// 								cursor: "pointer",
+		// 							},
+		// 						"&:hover .MuiOutlinedInput-notchedOutline": {
+		// 							border: "none",
+		// 							cursor: "pointer",
+		// 						},
+		// 					}}
+		// 					id="select-dropdown-small"
+		// 					value={activeSlide}
+		// 					onChange={handleTeamChange}
+		// 					IconComponent={MyIcon}
+		// 				>
+		// 					<MenuItem
+		// 						value={"All Teams"}
+		// 						onClick={() => {
+		// 							setActive("All Teams");
+		// 						}}
+		// 					>
+		// 						{"All Teams"}
+		// 					</MenuItem>
+		// 					{navObjects.map((navObject, index) => (
+		// 						<MenuItem
+		// 							key={index}
+		// 							value={navObject.title}
+		// 							onClick={() => {
+		// 								setActive(navObject.title);
+		// 							}}
+		// 						>
+		// 							{navObject.title}
+		// 						</MenuItem>
+		// 					))}
+		// 				</Select>
+		// 			</FormControl>
+		// 			<FormControl
+		// 				id="select-form"
+		// 				sx={{
+		// 					width: "100%",
+		// 				}}
+		// 			>
+		// 				<Select
+		// 					sx={{
+		// 						color: "white",
+		// 						".MuiOutlinedInput-notchedOutline": {
+		// 							border: "none",
+		// 							cursor: "pointer",
+		// 						},
+		// 						"&.Mui-focused .MuiOutlinedInput-notchedOutline":
+		// 							{
+		// 								border: "none",
+		// 								cursor: "pointer",
+		// 							},
+		// 						"&:hover .MuiOutlinedInput-notchedOutline": {
+		// 							border: "none",
+		// 							cursor: "pointer",
+		// 						},
+		// 					}}
+		// 					id="select-dropdown"
+		// 					value={status}
+		// 					onChange={handleChange}
+		// 					IconComponent={MyIcon}
+		// 				>
+		// 					<MenuItem
+		// 						value={"all"}
+		// 						onClick={() => {
+		// 							setShowAll(true);
+		// 							setShowCurrent(false);
+		// 							setShowPast(false);
+		// 						}}
+		// 					>
+		// 						All Team Members
+		// 					</MenuItem>
+		// 					<MenuItem
+		// 						value={"current"}
+		// 						onClick={() => {
+		// 							setShowAll(false);
+		// 							setShowCurrent(true);
+		// 							setShowPast(false);
+		// 						}}
+		// 					>
+		// 						Current Team Members
+		// 					</MenuItem>
+		// 					<MenuItem
+		// 						value={"past"}
+		// 						onClick={() => {
+		// 							setShowAll(false);
+		// 							setShowCurrent(false);
+		// 							setShowPast(true);
+		// 						}}
+		// 					>
+		// 						Past Team Members
+		// 					</MenuItem>
+		// 				</Select>
+		// 			</FormControl>
+		// 		</Toolbar>
+		// 	</div>
+		// </div>
 	);
 };
 
