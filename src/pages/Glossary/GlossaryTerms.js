722b1ce336b272f06dec7c8668aa0c123214de33
fj tria <fjtria.dev@gmail.com>
2025-02-25 00:33:47 -0800

    update glossary terms

---
+++ src/pages/Glossary/GlossaryTerms.js
@@ -0,0 +1,277 @@
+/**
+ * GlossaryTerms.js
+ * For Glossary.jsx, exports a list containing glossary entries as objects, each with a term and associated definition.
+ */
+
+export const glossaryTerms = [
+    {
+        term: "Astrometric Solution",
+        definition: "A technique in astronomical imaging that matches starts in " +
+                    "an image to a a star catalog. By analysing star positions, " +
+                    "angles, and distances, the image can be linked to a known " +
+                    "location in the sky. Originally done by hand, this process is " +
+                    'now automated with software "solvers." An astrometric solution' +
+                    "provides a model pinpointing each pixel's astronomical position."
+    },
+    {
+        term: "Black Body",
+        definition: "An idealized object that absorbs all incoming radiation and can " +
+                    "emit radiation at any frequency. Objects emit electromagnetic " +
+                    "waves depending on their temperature: colder objects emit lower-" +
+                    "energy waves (e.g., radio waves), while hotter objects emit " +
+                    "higher-energy waves (e.g., visible light). This is called black " +
+                    "body radiation, describing how an object's temperature relates to " +
+                    "the wavelengths it emits. Stars, like our Sun, approximate black " +
+                    "bodies, with their color determined by temperature. The Sun's peak " +
+                    "emission is in the green spectrum, but it emits a mix of red, " +
+                    "green, and blue light, which we see as white. Plans appear green " +
+                    "because they reflect this intense green wavelength, protecting " +
+                    "themselves from excess energy by absorbing less intense " +
+                    "wavelengths instead."
+
+    },
+    {
+        term: "Disintegrating Planets",
+        definition: "When a planet is losing dust and gas as it appear to (or " +
+                    "actually does) disintegrate, which can result in the light from " +
+                    "a star behind an orbiting planet becomming dimmed or blocked."
+
+    },
+    {
+        term: "Eccentricity",
+        definition: "A measure of how much the orbit of an orbiting body deviates from " +
+                    "a circle. An eccentricity of 0 would be a perfectly circular orbit. " +
+                    "An eccentricity of 1 is a parabolic orbit. Anything in between is " +
+                    "an elliptical orbit."
+    },
+    {
+        term: "Eclipsing Binaries",
+        definition: "A binary star system consists of two stars orbiting each other, " + 
+                    "bound by gravity. In an eclipsing binary, brightness varies as one " +
+                    "star passes in front of the other, temporarily blocking its light. " + 
+                    "This causes a dip in their combined brightness, where the large " +
+                    "dip occurs when the larger star eclipses the smaller one."
+    },
+    {
+        term: "Effective Temperature",
+        definition: "The temperature a black " +
+                    "body would have if it emitted the same luminosity per surface area " +
+                    "as the star (per Stefan-Boltzmann Law). It represents the star's " +
+                    "surface temperature if it were a perfect black body, fully " +
+                    "absorbing and re-emitting all incident light."
+    },
+    {
+        term: "G-Dwarf Star",
+        definition: "Or yellow dwarf, is a main sequence star like our Sun, capable " +
+                    "of fusing hydrogen into helium. Stars form when massive gas " +
+                    "clouds collapse under gravity, creating intense heat. If hot " +
+                    "enough, hydrogen fusion begins, releasing energy as light and " +
+                    "heat. This fusion process produces outward pressure, balancing " +
+                    "the inward pull of gravity. The hotter the star, the more fusion " +
+                    "occurs, eventually forming heavier elements up to iron. Elements " +
+                    "heavier than iron require a supernova to form. A main sequence " +
+                    "star is still actively fusing hydrogen and hasn't yet started " +
+                    "collapsing under its own gravity."
+    },
+    {
+        term: "Galactic Plane",
+        definition: "The plane through which the majority of a disk-shaped " +
+                    "galaxy's mass lies, typically referring specifically to the " +
+                    "Milky Way Galaxy. Directions perpendicular to galactic plane " +
+                    "define the galactic poles."
+    },
+    {
+        term: "Gravitational Lensing",
+        definition: "When light from a star located behind a black hole or other " +
+                    "massive object is bent around the massive object by its gravity. " +
+                    "This distorts the image of the star while allowing you to see " +
+                    "it even though it exists behind another object in the sky. In " +
+                    "some cases it can result in multiple images of the same object " +
+                    "appearing in the region around a black hole."
+    },
+    {
+        term: "Hot Jupiter",
+        definition: "A class of giant exoplanets that resemble Jupiter and have " +
+                    "very short orbital periods, meaning that they are orbiting " +
+                    "much closer to their star. According to CalTech, orbital periods " +
+                    "of Hot Jupiters are usually less than or equal to 10 days. They " +
+                    "are easy to identify using transit methods because of their " +
+                    "proximity to stars."
+    },
+    {
+        term: "Hydrogen-Helium Envelope",
+        definition: 'Is like the "envelope" of gases surrounding gas giants, and ' +
+                    "describes the pockets of gas surrounding certain types of exoplanets."
+    },
+    {
+        term: "Insolation Flux",
+        definition: "The amount of solar radiation an exoplanet receives. In astronomy, " +
+                    '"flux" refers to the flow of radiation through a surface' +
+                    "Insolation flux is the solar energy hitting a planet's surface " +
+                    "over a given time. On Earth, this varies with the season and, " +
+                    "at ground level, with weather and cloud cover."
+    },
+    {
+        term: "Limb Darkening",
+        definition: "Optical affect seen in stars where the outer edge of the star " +
+                    "appears dimmer or darker, while the center of the star appears " +
+                    "brighter. The darker outer edge is referred to as the stellar limb."
+    },
+    {
+        term: "log g (CGS)",
+        definition: "log g, in units of CGS, describes the surface gravity on an " +
+                    "exoplanet, otherwise known as the acceleration due to gravity. " +
+                    'On Earth, the gravitational constant "g" is 9.81 meters per ' + 
+                    "second squared (m/s^2), meaning that when something falls it's " +
+                    "speed is increasing as it falls by 9.81 meters per second every " +
+                    "second. On the moon, where gravity is weaker, the gravitational " +
+                    "constant is smaller. log g allows us to describe the " +
+                    "gravitational constant of exoplanets/other planets and " +
+                    'planetary objects (where "log" is a mathematical method of ' +
+                    "scaling values)."
+    },
+    {
+        term: "Main Sequence Star",
+        definition: "A star that can still fuse hydrogen into helium, producing " +
+                    "energy that counteracts its gravitational collapse. Stars " +
+                    "form from massive clouds of gas and dust that collapse under " +
+                    "gravity, generating heat from friction. If the cloud is large " +
+                    "and hot enough, fusion begins, releasing light and heat. This " +
+                    "energy creates outward pressure that balances gravity, " +
+                    "preventing the star from collapsing. Hotter stars fuse heavier " +
+                    "elements like helium into lithium and sodium, eventually " +
+                    "forming iron. Elements heavier than iron require supernova " +
+                    "energy to form. A main sequence star remains stable, actively " +
+                    "fusing hydrogen without collapsing."
+    },
+    {
+        term: "Mutual Inclination",
+        definition: "The tilt of an orbit around a celestial body with reference " +
+                    "to a particular and arbitrarily chosen plane. Mutual " +
+                    "inclination, therefore, is a measure of the inclinations of " +
+                    "two orbiting bodies."
+    },
+    {
+        term: "Orbital Inclination",
+        definition: "The tilt of an object's orbit around a " +
+                    "celestial body, expressed as the angle between a reference " +
+                    "plane and the axis of direction of the orbiting object, where " +
+                    "the reference plane is an arbitrary choice."
+    },
+    {
+        term: "Photoevaporation",
+        definition: "The process in which radiation ionises gas and causes it to " +
+                    "disperse away from the ionising source of the radiation. An " +
+                    "example of this is the removal of a planet's atmosphere due to " +
+                    'the high-energy photons ("packets" of light) emitted by nearby ' +
+                    "stars. The gas in the atmosphere is ionized, meaning electrons " +
+                    "are removed from the gas. The same process also occurs when " +
+                    "hot stars are close to gas clouds in space."
+    },
+    {
+        term: "Relativistic Beaming",
+        definition: "A frame of reference is a stated coordinate system or set of " +
+                    "directions in which measurements are made. In relativistic " +
+                    "frames (frames of reference that are moving close to the speed " +
+                    "of light), relativistic beaming is when a light source is " +
+                    "emitted from an object in a frame of reference that is moving " +
+                    "at relativistic speed relative to another object, therefore " +
+                    "shifting the appearance of the light. Therefore, if a star " +
+                    "were moving past you at near the speed of light, it's light " +
+                    "would appear to become focused in the direction of motion, " +
+                    "whereas if you were at rest relative to that star (meaning " +
+                    "that you were traveling alongside it at the same speed and in " +
+                    "the same direction) it's light would emit in all directions."
+    },
+    {
+        term: "Scale Heights",
+        definition: "Scale height refers to the distance over which a physical " +
+                    "variable decreases by a factor of e (e being a naturally " +
+                    "occurring number with a value of approximately 2.718). In " +
+                    "terms of a galaxy, the scale height describes the height above " +
+                    'the galactic disk/ galactic plane "at which the density of a ' +
+                    'particular constituent of the disk has declined by e". In the ' +
+                    "study of planetary atmospheres, the scale height is the " +
+                    "altitude at which the atmospheric pressure decreases by a " +
+                    "factor of e (2.718). On Earth, the scale height is 8.5 " +
+                    "kilometers, which means that at an altitude of 8.5 kilometers " +
+                    "the atmospheric pressure has decreased by 2.718."
+    },
+    {
+        term: "Secondary Eclipse",
+        definition: "When a planet passes behind a star and the light from the planet is dimmed."
+    },
+    {
+        term: "Stellar Disk",
+        definition: "The ring-shaped accumulation of matter that orbits around a star."
+    },
+    {
+        term: "Stellar Limb",
+        definition: "The outer edge of a star, which appears darker due to the effect of limb darkening."
+    },
+    {
+        term: "Stellar Variability",
+        definition: "Describes how stars change throughout their lifetimes, such " +
+                    "as variations in brightness and size depending on their " +
+                    "position in the sequence of star-lives."
+    },
+    {
+        term: "Super Earths",
+        definition: "Exoplanets whose mass is higher than Earth's, but still " +
+                    "significantly smaller than that of the ice giants."
+    },
+    {
+        term: "Three Body Resonance",
+        definition: "Orbital resonance is when orbiting bodies exert periodic " +
+                    "gravitational influence on each other during orbits. " +
+                    "Three-body resonance describes when orbital resonance occurs " +
+                    "between three orbiting bodies (such as in the infamous " +
+                    "three-body problem)."
+    },
+    {
+        term: "Threshold Crossing Events (TCE)",
+        definition: "A potentially periodic decrease in the brightness of a star " +
+                    "that has been detected in a search for exoplanets using a light curve."
+    },
+    {
+        term: "Tidal Circularization",
+        definition: "The process in which the orbit of an orbiting body gradually " +
+                    "becomes more circular over time. It's an effect of the tidal " +
+                    "forces between an orbiting body and the object that it orbits, " +
+                    "where the eccentricity (measure of how much an orbit deviates " +
+                    "from a circle) decreases over time."
+    },
+    {
+        term: "Transit Depth",
+        definition: "The ratio of the surface area of the " +
+                    "star's disk blocked out by the planet's disk. In other words, " +
+                    "the ratio of the area of the planet to the area of the star, " +
+                    "which can be used to calculate the size of the planet."
+    },
+    {
+        term: "Transit Midpoint",
+        definition: "The average of the time from when the exoplanet begins to " +
+                    "cross the stellar limb to the time the candidate finishes " +
+                    "crossing the stellar limb."
+    },
+    {
+        term: "Transit Timing Variation",
+        definition: "A method for detecting exoplanets by observing variations in " +
+                    "the timing of a transit. It is based on the idea that when a " +
+                    "planet crosses a star and gets tugged on by a second planet, " +
+                    "it will speed up or slow down, and therefore won't cross the " +
+                    "star when predicted. The difference in timing allows for the " +
+                    "prediction of a second planet."
+    },
+    {
+        term: "Transit",
+        definition: "An object or exoplanet passing in front of a star and dimming its light."
+    },
+    {
+        term: "Warm Jupiter",
+        definition: "A type of planet classified as being between Jupiter-like " +
+                    "planets and Hot Jupiters, in which their orbital periods are " +
+                    "between that of Jupiters (over 100 days) and Hot Jupiters (1 " +
+                    "to 10 days)."
+    },
+  ];
